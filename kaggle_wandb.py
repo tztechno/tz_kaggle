c5e2cfaa35fda1fd878231d396f7d1a576aac5aa
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-18 00:41:08 +0900

    Create kaggle_wandb.py

---
+++ kaggle_wandb.py
@@ -0,0 +1,17 @@
+# Import wandb library for logging and tracking experiments
+import wandb
+
+# Try to get the API key from Kaggle secrets
+try:
+    from kaggle_secrets import UserSecretsClient
+    user_secrets = UserSecretsClient()
+    api_key = user_secrets.get_secret("WANDB")
+    # Login to wandb with the API key
+    wandb.login(key=api_key)
+    # Set anonymous mode to None
+    anonymous = None
+except:
+    # If Kaggle secrets are not available, set anonymous mode to 'must'
+    anonymous = 'must'
+    # Login to wandb anonymously and relogin if needed
+    wandb.login(anonymous=anonymous, relogin=True)
