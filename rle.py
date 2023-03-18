bbf6ef77640b79da8ef8e4d881aa859ee7f938f1
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-18 19:36:29 +0900

    Update rle.py

--- rle.py
+++ rle.py
@@ -8,3 +8,15 @@ def rle(output):
     ends_ix = np.where(ends)[0] + 2
     lengths = ends_ix - starts_ix
     return " ".join(map(str, sum(zip(starts_ix, lengths), ())))
+
+
+def decode_RLE(rle_string, shape):
+    rle_numbers = np.asarray([int(numstring) for numstring in rle_string.split()])
+    starts, lengths = rle_numbers[0::2], rle_numbers[1::2]
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 255
+    img = img.reshape(shape).T
+    return img
+
