e1eb4d4cc181adc8cc8c253311abeb5fc3b4dbe7
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-18 22:20:32 +0900

    Update create_animation.py

--- create_animation.py
+++ create_animation.py
@@ -12,3 +12,19 @@ def create_animation(ims):
         return [im]
 
     return animation.FuncAnimation(fig, animate_func, frames=len(ims), interval=1000/10)
+
+
+
+def create_animation_with_text(ims):
+    fig = plt.figure(figsize=(4, 4))
+    plt.axis('off')
+    im = plt.imshow(ims[0])
+    text = plt.text(0.05, 0.05, f'Slide {0}', transform=fig.transFigure, fontsize=14)
+    plt.close()
+
+    def animate_func(i):
+        im.set_array(ims[i])
+        text.set_text(f'Slide {i}')
+        return [im, text]
+
+    return animation.FuncAnimation(fig, animate_func, frames=len(ims), interval=1000/10)
