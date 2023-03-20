37681293c3db6b28ac946b045921d00a5c89577d
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-20 19:57:28 +0900

    Update convert_to_input_tensor.py

--- convert_to_input_tensor.py
+++ convert_to_input_tensor.py
@@ -7,4 +7,28 @@ def convert_to_input_tensor(frames):
     frames_tensor = torch.from_numpy(frames).float()  # NumPy配列をPyTorchのTensorに変換
     input_tensor = frames_tensor.unsqueeze(0)  # バッチサイズ1の4次元テンソルに変換
     return input_tensor
+
+
+
+
+import torch
+import numpy as np
+from PIL import Image
+
+def convert_to_input_tensor(paths, H=64, W=64):
+    frames = []
+    for path in paths:
+        # 画像ファイルを読み込み、サイズを変更してからリストに追加
+        img = Image.open(path).resize((W, H))
+        frame = np.array(img, dtype=np.float32) / 255.0  # ピクセル値を[0, 1]に正規化
+        frames.append(frame)
+
+    # リストをNumPy配列に変換し、テンソルに変換してから軸を変換
+    frames = np.array(frames)
+    frames = np.transpose(frames, (0, 3, 1, 2))
+
+    # バッチサイズ1の4次元テンソルに変換
+    input_tensor = torch.from_numpy(frames).float().unsqueeze(0)
+
+    return input_tensor
     
