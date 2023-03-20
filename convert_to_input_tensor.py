2f764ac77547d606a891b65ee55a8fe4d04b0608
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-20 20:11:13 +0900

    Update convert_to_input_tensor.py

--- convert_to_input_tensor.py
+++ convert_to_input_tensor.py
@@ -15,7 +15,7 @@ import torch
 import numpy as np
 from PIL import Image
 
-def convert_to_input_tensor(paths, H=64, W=64):
+def convert_to_input_tensor(paths, H=64, W=64, D=32):
     frames = []
     for path in paths:
         # 画像ファイルを読み込み、サイズを変更してからリストに追加
@@ -27,8 +27,12 @@ def convert_to_input_tensor(paths, H=64, W=64):
     frames = np.array(frames)
     frames = np.transpose(frames, (0, 3, 1, 2))
 
-    # バッチサイズ1の4次元テンソルに変換
+    # バッチサイズ1の5次元テンソルに変換
     input_tensor = torch.from_numpy(frames).float().unsqueeze(0)
 
+    # 3次元畳み込み層を適用するためのdepthの指定
+    input_tensor = input_tensor.permute(0, 2, 1, 3, 4)
+    input_tensor = input_tensor.reshape(1, D, 3, H, W)
+
     return input_tensor
-    
+
