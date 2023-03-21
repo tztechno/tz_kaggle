ff1121d91630cf81ea04a0b7d4f8dd920259ac73
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-21 23:50:11 +0900

    Create Conv2dModel.py

---
+++ Conv2dModel.py
@@ -0,0 +1,69 @@
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+  
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                   # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+        
+        
+class CnnModel(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(3, 100, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(100, 150, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), 
+
+            nn.Conv2d(150, 200, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(200, 200, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), 
+
+            nn.Conv2d(200, 250, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(250, 250, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), 
+
+            nn.Flatten(), 
+            nn.Linear(6250, 256),  
+            nn.ReLU(),            
+            nn.Linear(256, 128),  
+            nn.ReLU(),            
+            nn.Linear(128, 64),           
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(32, len(classes)))
+        
+    def forward(self, xb):
+        return self.network(xb)
+      
+      
