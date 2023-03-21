25c8266b19ac92bf91d3df90dd931f3970b221a9
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-21 17:21:24 +0900

    Update Conv3dModel.py
    shapes are wrong

--- Conv3dModel.py
+++ Conv3dModel.py
@@ -48,3 +48,66 @@ output_tensor = model(input_tensor)
 
 # 出力データのテンソルのサイズ(batch_size, out_channels, out_depth, out_height, out_width)
 print(output_tensor.shape)
+
+
+
+
+# other sample
+class Model1(nn.Module):
+    def __init__(self, input_shape):
+        nn.Module.__init__(self)
+        
+        self.input_shape = input_shape
+        
+        self.conv = nn.ModuleList(
+            [nn.BatchNorm3d(self.input_shape[0]),
+             nn.Conv3d(self.input_shape[0], 64, 3, padding=1), 
+             nn.ReLU(), 
+             nn.MaxPool3d(2),
+             nn.Dropout3d(),
+                
+             nn.Conv3d(64, 128, 3, padding=1), 
+             nn.ReLU(), 
+             nn.MaxPool3d(2), 
+             nn.Dropout3d(),
+                
+             nn.Conv3d(128, 256, 3, padding=1), 
+             nn.ReLU(), 
+             nn.MaxPool3d(2),
+             nn.Dropout3d()])
+        
+        #conv_shape = _output_shape(self.input_shape[1:], self.conv)
+        conv_shape = self._infer_shape()
+        
+        self.fc = nn.ModuleList(
+            [nn.Linear(int(conv_shape.prod()), 1024), 
+             nn.ReLU(),
+             nn.Dropout3d(),
+             
+             nn.Linear(1024, 1)])
+        
+        map(_init_fun, self.conv)
+        map(_init_fun, self.fc)
+        
+    def _infer_shape(self):
+        x = torch.randn(tuple([1] + map(int, self.input_shape)))
+        logging.info('Inferring shape for convolutional part...')
+        logging.info(x.shape)
+        
+        for l in self.conv:
+            x = l(x);
+            logging.info("==== " + str(l) + " ====")
+            logging.info(x.shape)
+        
+        return torch.tensor(x.shape)
+    
+    def forward(self, x):
+        for l in self.conv:
+            x = l(x)
+        x = x.view(x.size()[0], -1)
+        for l in self.fc:
+            x = l(x)
+        x = torch.sigmoid(x)
+        return x   
+    
+    
