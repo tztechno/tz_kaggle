5be7d4091ccf97dd18041afe50cf737ec5e2bdcd
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-21 23:46:43 +0900

    Update Conv3dModel.py

--- Conv3dModel.py
+++ Conv3dModel.py
@@ -24,6 +24,7 @@ print(output_tensor.shape)
 
 
 
+###########################################################################
 
 # model for movie
 import torch.nn as nn
@@ -51,6 +52,7 @@ print(output_tensor.shape)
 
 
 
+###########################################################################
 
 # other sample
 class Conv3DNet(nn.Module):
@@ -122,4 +124,87 @@ class Conv3DNet(nn.Module):
         return 
 
 
+###########################################################################
 
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        h_dim = [32, 64, 128]
+        self.net = nn.Sequential(
+            nn.Conv3d(1, h_dim[0], 4, 2, 1), # 128, 128, 50
+            nn.BatchNorm3d(h_dim[0]),
+            nn.Dropout(0.05),
+            nn.MaxPool3d((2,2,2)),  # 64, 64,50
+            nn.Conv3d(h_dim[0], h_dim[1], 4, 2, 1), # 32, 32,12
+            nn.BatchNorm3d(h_dim[1]),
+            nn.Dropout(0.05),
+            nn.MaxPool3d((2,2,2)),   # 16, 16,12
+            nn.Conv3d(h_dim[1], h_dim[2], 4, 2, 1),  # 8, 8, 3
+            nn.BatchNorm3d(h_dim[2]),
+            nn.Dropout(0.05),
+            nn.MaxPool3d((4,4,1)), # 2, 2, 3
+            View((-1, h_dim[-1]*2*2*3)),
+            nn.Linear(h_dim[-1]*2*2*3, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 1))
+    
+    def forward(self, x):
+        #x: [bz, 1, 256, 256, 400]
+        x = self.net(x)
+        x = torch.sigmoid(x)
+        eps = 1e-6
+        x = 0.5*eps + (1-eps)*x
+        return x
+    
+    
+    
+###########################################################################
+
+def get_model(width=128, height=128, depth=64):
+    """Build a 3D convolutional neural network model."""
+
+    inputs = keras.Input((width, height, depth, 1))
+
+    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.GlobalAveragePooling3D()(x)
+    x = layers.Dense(units=512, activation="relu")(x)
+    x = layers.Dropout(0.3)(x)
+
+    outputs = layers.Dense(units=1, activation="sigmoid")(x)
+
+    # Define the model.
+    model = keras.Model(inputs, outputs, name="3dcnn")
+    return model
+
+
+
+###########################################################################
+
+model = Sequential()
+model.add(Conv3D(64,(3,3,3), input_shape=(16,16,16,3), padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(MaxPool3D(pool_size=(2, 2, 2)))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+ 
+
+###########################################################################   
+  
+    
