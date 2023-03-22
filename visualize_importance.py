bb7529dda5d26e2070c5778b10c1e723b89f5c28
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-22 15:24:10 +0900

    Create visualize_importance.py

---
+++ visualize_importance.py
@@ -0,0 +1,36 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def visualize_importance(models, feat_train_df):
+
+    feature_importance_df = pd.DataFrame()
+    for i, model in enumerate(models):
+        _df = pd.DataFrame()
+        _df['feature_importance'] = model.feature_importances_
+        _df['column'] = feat_train_df.columns
+        _df['fold'] = i + 1
+        feature_importance_df = pd.concat([feature_importance_df, _df], 
+                                          axis=0, ignore_index=True)
+
+    order = feature_importance_df.groupby('column')\
+        .sum()[['feature_importance']]\
+        .sort_values('feature_importance', ascending=False).index[:50]
+
+    fig, ax = plt.subplots(figsize=(8, max(6, len(order) * .25)))
+    sns.boxenplot(data=feature_importance_df, 
+                  x='feature_importance', 
+                  y='column', 
+                  order=order, 
+                  ax=ax, 
+                  palette='viridis', 
+                  orient='h')
+    
+    ax.tick_params(axis='x', rotation=0)
+    ax.set_title('Importance')
+    ax.grid()
+    fig.tight_layout()
+    
+    return fig,ax
+
+fig, ax = visualize_importance(models, train_feat_df)
