0cc0aef0b8446a8f4b2fff3e26fee17714ca4a86
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-22 16:30:42 +0900

    Create labelencoder.py

---
+++ labelencoder.py
@@ -0,0 +1,11 @@
+
+from sklearn.preprocessing import LabelEncoder
+
+def labelencoder(df):
+    for c in df.columns:
+        if df[c].dtype=='object': 
+            df[c] = df[c].fillna('N')
+            lbl = LabelEncoder()
+            lbl.fit(list(df[c].values))
+            df[c] = lbl.transform(df[c].values)
+    return df
