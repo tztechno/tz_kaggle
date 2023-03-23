b5deec1fb56eeed9ed07ab92108a6ec2220dfba6
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-23 13:29:31 +0900

    Create random_subplots.py

---
+++ random_subplots.py
@@ -0,0 +1,15 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ランダムに9個のデータを選ぶ
+indices = np.random.choice(len(dataX), size=9, replace=False)
+
+# 3x3のグリッドに表示する
+fig, axs = plt.subplots(3, 3, figsize=(8, 8))
+for i, ax in enumerate(axs.flatten()):
+    ax.imshow(dataX[indices[i]])
+    ax.set_title(str(dataY[indices[i]]))
+    ax.axis('off')
+plt.tight_layout()
+plt.show()
