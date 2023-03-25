ffd78b2ae26e4f001a97d0462019b3a6d8187fda
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-25 14:59:25 +0900

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -17,3 +17,32 @@ labels['label']=label
 labels.to_csv('labels.csv',index=False)
 
 ///////////////////////////////////////////
+
+!mkdir train
+!mkdir test
+label = ['a','b','c']
+for L in label:
+    !mkdir train/{L}
+    !mkdir test/{L}
+
+#label=['a','b','c']
+dirs=['train','test']
+N,C,H,W = 400,3,100,100
+
+paths=[]
+for i in range(400):
+    k=i//300
+    p=i%3
+    datai = torch.randint(low=0, high=256, size=(3,100,100), dtype=torch.uint8)
+    path=f'./{dirs[k]}/{label[p]}/'+str(i).zfill(3)+'.pt'
+    paths+=[path]
+    torch.save(datai,path)
+
+label = np.random.randint(0,3,size=(400,))
+labels = pd.DataFrame(columns=['id','label'])
+labels['id']=list(range(400))
+labels['label']=label
+labels.to_csv('labels.csv',index=False)
+display(labels[0:5])
+
+///////////////////////////////////////////
