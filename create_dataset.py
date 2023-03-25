944450ab15b60ce9cb5d1a0b5f5955e1732d049c
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-25 13:33:38 +0900

    Create create_dataset.py

---
+++ create_dataset.py
@@ -0,0 +1,19 @@
+///////////////////////////////////////////
+
+N,C,D,H,W = 60,3,10,100,100
+paths=[]
+for i in range(600):
+    r=i//10
+    c=i%10
+    datai = torch.randint(low=0, high=256, size=(3,100,100), dtype=torch.uint8)
+    path='./data/'+str(r).zfill(3)+'_'+str(c).zfill(3)+'.pt'
+    paths+=[path]
+    torch.save(datai,path)
+label = [0,1,2]
+label = np.random.randint(0,3,size=(60,))
+labels = pd.DataFrame(columns=['id','label'])
+labels['id']=list(range(60))
+labels['label']=label
+labels.to_csv('labels.csv',index=False)
+
+///////////////////////////////////////////
