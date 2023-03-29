00670c41e3910ceaa729a31f6d5350a554da59cc
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-29 22:14:46 +0900

    Create mapping.py

---
+++ mapping.py
@@ -0,0 +1,13 @@
+################
+
+mapping_dict = movies.set_index("Movie_ID")["Name"].to_dict()
+
+################
+
+Name=data['Species'].unique().tolist()
+N=list(range(len(Name)))
+normal_mapping=dict(zip(Name,N)) 
+reverse_mapping=dict(zip(N,Name))       
+data['Species']=data['Species'].map(normal_mapping)
+
+################
