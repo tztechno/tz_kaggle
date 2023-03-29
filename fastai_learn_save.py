847147b1bf7232c031f928d137403da35e6d2514
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-29 14:33:06 +0900

    Update fastai_learn_save.py

--- fastai_learn_save.py
+++ fastai_learn_save.py
@@ -1,34 +1,22 @@
-################################### first time
+################################### 
+### first time
 
 learn = vision_learner(dls, 'resnet26', metrics=error_rate, path='.').to_fp16()
 
 learn.lr_find(suggest_funcs=(valley, slide))
 
-learn.fine_tune(3,0.01)  ### ここで学習
+learn.fine_tune(3,0.01)  
 
-learn.save('my_model')   ### 保存してみる
+learn.save('my_model')   
 
-################################### 繰り返し学習
+################################### 
+### second time and after
 
 learn = load_learner('my_model')
 
-dls_new = # 新しいデータを読み込むためのDataLoadersを定義する
-
 learn.dls = dls_new
 
-learn.fine_tune(3, 0.01)  ### さらに詰め込み学習
+learn.fine_tune(3, 0.01) 
 
-#################################### 分散学習
 
-from fastai.learner import load_learner
-from fastai.ensemble import *
-
-learn.export('model1.pkl')
-
-model1 = load_learner('model1.pkl')
-model2 = load_learner('model2.pkl')
-learners = [model1, model2]
-
-ensemble_model = ensemble(learners)
-ensemble_model.predict('test.jpg')
 
