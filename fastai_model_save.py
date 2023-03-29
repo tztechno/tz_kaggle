04cbf384d17dc8f523a38d3f33e955363cbbb792
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-30 01:20:54 +0900

    Update fastai_model_save.py

--- fastai_model_save.py
+++ fastai_model_save.py
@@ -2,21 +2,20 @@
 ### first time
 
 learn = vision_learner(dls, 'resnet26', metrics=error_rate, path='.').to_fp16()
-
 learn.lr_find(suggest_funcs=(valley, slide))
-
 learn.fine_tune(3,0.01)  
-
-learn.save('my_model')   
+torch.save(learn.state_dict(), 'model.pt')
 
 ################################### 
 ### second time and after
 
-learn = load_learner('my_model')
+from fastai.vision.learner import create_cnn
 
-learn.dls = dls_new
+model_path = '/kaggle/input/asl-slice-images-fk0-first-fastai2/model.pt'
+model = torch.load(model_path, map_location=torch.device('cpu'))
+learn = create_cnn(dls, 'resnet26', pretrained=False) #新しいデータはinput,学習はまだ
+learn.model.load_state_dict(model) #標準形にこれまでの学習結果を足す
+learn.fine_tune(3,0.01)  
+torch.save(learn.state_dict(), 'model.pt')
 
-learn.fine_tune(3, 0.01) 
-
-
-
+################################### 
