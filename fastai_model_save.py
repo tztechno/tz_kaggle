eabd43ecdb3f97ab34429b7d666707a298612e1d
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-30 01:35:51 +0900

    Update fastai_model_save.py

--- fastai_model_save.py
+++ fastai_model_save.py
@@ -13,7 +13,7 @@ from fastai.vision.learner import cnn_learner
 
 model_path = '/kaggle/input/asl-slice-images-fk0-first-fastai2/model.pt'
 model = torch.load(model_path, map_location=torch.device('cpu'))
-learn = create_cnn(dls, 'resnet26', pretrained=False) #新しいデータはinput,学習はまだ
+learn = cnn_learner(dls, 'resnet26', pretrained=False) #新しいデータはinput,学習はまだ
 learn.model.load_state_dict(model) #モデル標準形にこれまでの学習結果を足す
 learn.fine_tune(3,0.01)  
 torch.save(learn.state_dict(), 'model.pt')
