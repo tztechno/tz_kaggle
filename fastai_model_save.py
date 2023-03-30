e5ff20cc04d73b1ee08785e07f8a1cb3ecafff74
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-30 19:42:40 +0900

    Update fastai_model_save.py

--- fastai_model_save.py
+++ fastai_model_save.py
@@ -3,6 +3,7 @@
 
 learn = vision_learner(dls, 'resnet26', metrics=error_rate, path='.').to_fp16()
 learn.lr_find(suggest_funcs=(valley, slide))
+
 learn.fine_tune(3,0.01)  
 torch.save(learn.state_dict(), 'model.pt')
 
@@ -15,6 +16,8 @@ model_path = '/kaggle/input/asl-slice-images-fk0-first-fastai2/model.pt'
 model = torch.load(model_path, map_location=torch.device('cpu'))
 learn = cnn_learner(dls, 'resnet26', pretrained=False) #新しいデータinput
 learn.model.load_state_dict(model) #baseモデルにトレーニング結果を反映
+learn.lr_find(suggest_funcs=(valley, slide))
+
 learn.fine_tune(3,0.01)  
 torch.save(learn.state_dict(), 'model.pt')
 
@@ -27,3 +30,16 @@ torch.onnx.export(learn.model, torch.randn(batch_size, *input_shape), "model.onn
 
 ################################### 
 
+# onnxをロードする場合
+import numpy as np
+import onnxruntime
+
+learn = cnn_learner(dls, 'resnet26', metrics=error_rate, path='.', pretrained=False)
+ort_session = onnxruntime.InferenceSession(model_path)
+ort_state_dict = {}
+for i, name in enumerate(ort_session.get_outputs()):
+    ort_state_dict[name.name] = ort_session.get_outputs()[i].detach().cpu().numpy()
+learn.model.load_state_dict(ort_state_dict)
+learn.lr_find(suggest_funcs=(valley, slide))
+
+################################### 
