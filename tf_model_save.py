ee0e76646b2329124e1d05354535aa7a6b6b105d
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-01 13:10:24 +0900

    Update tf_model_save.py

--- tf_model_save.py
+++ tf_model_save.py
@@ -76,28 +76,3 @@ with open('model.pkl', 'wb') as f:
 
 #############
 
-import torch
-import torch.nn as nn
-
-# モデルを定義する
-class MyModel(nn.Module):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        self.fc1 = nn.Linear(784, 128)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(128, 10)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.softmax(x)
-        return x
-
-model = MyModel()
-
-# PyTorch形式でモデルを保存する
-torch.save(model.state_dict(), 'model.pth')
-
-#############
