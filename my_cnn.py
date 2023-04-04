9ee70fbf4a2dfc34e6bd78a19e060a96381c4309
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-04 22:59:35 +0900

    Update my_cnn.py

--- my_cnn.py
+++ my_cnn.py
@@ -20,5 +20,29 @@ class MyCNN(nn.Module):
         x = self.fc2(x)
         return x
 
+#########################################################
+#input torch.Size([16, 3, 400, 400])
+#output torch.Size([16, 250])
+
+class MyCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(128 * 25 * 25, 256)
+        self.fc2 = nn.Linear(256, 250)
+
+    def forward(self, x):
+        x = self.pool1(nn.functional.relu(self.conv1(x)))
+        x = self.pool2(nn.functional.relu(self.conv2(x)))
+        x = self.pool3(nn.functional.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 25 * 25)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 ############################################################
