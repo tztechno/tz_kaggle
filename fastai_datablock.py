40b40e43a9bcc705c9348c86b05f775772e034ad
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-04 22:21:41 +0900

    Update fastai_datablock.py

--- fastai_datablock.py
+++ fastai_datablock.py
@@ -16,3 +16,21 @@ dls = dls.dataloaders(np_data)
 
 ####################################################
 
+from fastai.vision.all import *
+
+# 2次元numpy配列の準備
+x = np.random.rand(100, 100)
+y = np.random.randint(0, 2, size=(100, 100))
+
+# データセットの作成
+db = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
+               get_items=get_image_files,
+               splitter=RandomSplitter(),
+               get_y=lambda o: y,
+               item_tfms=[Resize(224, method='pad', pad_mode='zeros')],
+               batch_tfms=[*aug_transforms()])
+
+dls = db.dataloaders(x, bs=32)
+
+
+####################################################
