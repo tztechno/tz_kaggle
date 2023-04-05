17a8221c31660fe27e501e7147c2c551581f2be8
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-05 21:58:22 +0900

    Update my_cnn.py

--- my_cnn.py
+++ my_cnn.py
@@ -46,3 +46,40 @@ class MyCNN(nn.Module):
         return x
 
 ############################################################
+#input torch.Size([16, 3, 128, 128])
+#output torch.Size([16, 5])
+
+import torch.nn as nn
+
+class MyCNN(nn.Module):
+    def __init__(self):
+        super(MyCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(128 * 16 * 16, 512)
+        self.fc2 = nn.Linear(512, 5)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.dropout(x)
+        x = x.view(-1, 128 * 16 * 16)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        
+        return x
+
+
+############################################################
