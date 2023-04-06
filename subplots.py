31353e73a2f579a235003c8b553003e81eadcf8d
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-06 12:06:54 +0900

    Create subplots.py

---
+++ subplots.py
@@ -0,0 +1,12 @@
+######################################
+
+indices = np.random.choice(trainX.shape[0], 9)
+fig, axs = plt.subplots(3, 3, figsize=(8,8))
+for i, ax in enumerate(axs.flatten()):
+    ax.imshow(trainX[indices[i]].transpose(1,2,0))
+    ax.axis("off")
+    ax.set_title(normal_mapping[trainY[indices[i]]])
+plt.tight_layout()
+plt.show()
+
+######################################
