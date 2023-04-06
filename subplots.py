99096fec852f50b57a31b90dfa3bab6953b51e12
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-06 13:46:05 +0900

    Update subplots.py

--- subplots.py
+++ subplots.py
@@ -10,3 +10,15 @@ plt.tight_layout()
 plt.show()
 
 ######################################
+
+from PIL import Image
+fig, axes = plt.subplots(3, 3, figsize=(10, 10))
+for i, ax in enumerate(axes.ravel()):
+    if i < len(indices):
+        image = Image.fromarray(trainX[indices[i]])
+        ax.imshow(image)
+        ax.axis('off')
+        ax.set_title(normal_mapping[trainY[indices[i]]])
+plt.show()
+
+######################################
