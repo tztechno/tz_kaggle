4649e22035c5d6b9427be24dcfac079a797d2287
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-07 15:32:10 +0900

    Update copytree.py

--- copytree.py
+++ copytree.py
@@ -2,11 +2,27 @@
 import os
 import shutil
 
-//method1
+
+#method1
 if not os.path.exists(path1):
     shutil.copytree(path0, path1)
 
-//mehod2
+    
+#mehod2
 if os.path.exists(path1):
     shutil.rmtree(path1)
 shutil.copytree(path0, path1)
+
+
+#method3
+destination = "frame" 
+if not os.path.exists(destination):
+    os.makedirs(destination)
+
+for filename in os.listdir(path0):
+    src_path = os.path.join(path0, filename) 
+    if filename.endswith(".jpg"):
+        dst_path = os.path.join(destination, filename) 
+        shutil.copyfile(src_path, dst_path)  
+        
+        
