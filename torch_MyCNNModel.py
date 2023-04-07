5332caded755b1c1d295a2556e31b3e2f1085f17
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-07 17:33:13 +0900

    Update torch_MyCNNModel.py

--- torch_MyCNNModel.py
+++ torch_MyCNNModel.py
@@ -1,4 +1,5 @@
 ##############################################
+
 ## input (32, 224, 224, 3) 
 ## output (32, 5)
 
@@ -18,4 +19,23 @@ class MyCNNModel(nn.Module):
         x = F.relu(self.fc1(x))               # Fully connected layer 1 -> ReLU
         x = self.fc2(x)                       # Fully connected layer 2 (output)
         return x
-##############################################      
+    
+############################################## 
+
+## input (32, 784) 
+## output (32, 10)
+
+class MyCNN(nn.Module):
+    def __init__(self):
+        super(MyCNN, self).__init__()
+        self.conv1 = nn.Conv1d(1, 32, kernel_size=5, stride=1)
+        self.fc1 = nn.Linear(32*780, 10)
+
+    def forward(self, x):
+        x = x.view(x.size(0), 1, 32, 784)  # 入力データのサイズを変更
+        x = self.conv1(x)
+        x = x.view(x.size(0), -1)  # データを1次元に変換
+        x = self.fc1(x)
+        return x
+    
+ ##############################################    
