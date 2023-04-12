4a8412753ca4667081e686681acdc1808bfdcdd8
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-12 15:28:25 +0900

    Update one_hot_encoding.py

--- one_hot_encoding.py
+++ one_hot_encoding.py
@@ -1,14 +1,18 @@
 
+###########################
+
 import torch
 
-# 例としてのdataYのデータ
 dataY = torch.tensor([0, 1, 2, 1, 0])
-
-# One-Hot Encodingの実行
-num_classes = 3  # クラスの総数
-one_hot = torch.zeros(dataY.size(0), num_classes)  # サイズを(データ数, クラス数)のゼロ行列で初期化
-
-# scatterを使って、各データに対応するクラスの位置に1を設定する
+num_classes = 3
+one_hot = torch.zeros(dataY.size(0), num_classes)
 one_hot.scatter_(1, dataY.unsqueeze(1), 1)
 
 print(one_hot)
+
+###########################
+
+dataY0=np.array(dataY0).astype(np.int64)
+dataY = torch.eye(245)[dataY0]
+
+###########################
