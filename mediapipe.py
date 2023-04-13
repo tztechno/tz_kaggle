851ad480d56fde0beaa39e4dc030dd9d054eeb42
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-14 02:13:07 +0900

    Create mediapipe.py

---
+++ mediapipe.py
@@ -0,0 +1,12 @@
+#########################################
+#pose
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
+#########################################
+#hands
+    with mp_hands.Hands(
+        static_image_mode=True,
+        max_num_hands=2,
+        min_detection_confidence=0.1) as hands:
+##########################################
