bc626d5e77925f3d726bfd557a3b02b1009592eb
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-14 12:42:02 +0900

    Update mediapipe.py

--- mediapipe.py
+++ mediapipe.py
@@ -4,6 +4,13 @@ with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
 #########################################
+#pose    
+with mp_pose.Pose(
+    static_image_mode=True,
+    model_complexity=2,
+    enable_segmentation=True,
+    min_detection_confidence=0.5) as pose:    
+#########################################
 #hands
     with mp_hands.Hands(
         static_image_mode=True,
