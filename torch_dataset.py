679520ad88f9421c0d05b53edf1d2ef18dba3ee5
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-21 11:40:14 +0900

    Update torch_dataset.py

--- torch_dataset.py
+++ torch_dataset.py
@@ -1,4 +1,27 @@
 
+######################################################
+#create dataset from df of path/label
+
+import torch
+from torch.utils.data import Dataset
+
+class MyDataset(Dataset):
+    def __init__(self, data_df):
+        self.data_df = data_df
+    
+    def __len__(self):
+        return len(self.data_df)
+    
+    def __getitem__(self, idx):
+        path, label = self.data_df.iloc[idx]
+        img = Image.open(os.path.join(dir0,path)).convert('RGB')
+        img = img.resize((224, 224))
+        img = torch.tensor(np.array(img)).permute(2, 0, 1).float() / 255.0
+        
+        return img, label
+    
+######################################################
+
 import torch
 from torch.utils.data import Dataset
 
@@ -19,3 +42,6 @@ class MyDataset(Dataset):
 
 # データセットを作成する
 dataset = MyDataset(data, labels)
+
+######################################################
+
