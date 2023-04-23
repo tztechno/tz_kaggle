400fc20cb9e7c06bf0a8aa1790d088fc08198a75
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-23 12:21:21 +0900

    Update torch_Net.py

--- torch_Net.py
+++ torch_Net.py
@@ -4,6 +4,9 @@
 #(224,224)
 model= models.alexnet(pretrained=True)
 
+for param in model.parameters():
+    param.requires_grad=False
+
 model.classifier=nn.Sequential( nn.Linear(9216,1024),
                                 nn.ReLU(),
                                 nn.Dropout(p=0.5),
