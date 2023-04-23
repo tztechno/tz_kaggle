51af2e7125640bffc7cceac7f5667d8f04d43495
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-23 12:18:45 +0900

    Update torch_Net.py

--- torch_Net.py
+++ torch_Net.py
@@ -1,1 +1,13 @@
 
+###############################################################
+
+#(224,224)
+model= models.alexnet(pretrained=True)
+
+model.classifier=nn.Sequential( nn.Linear(9216,1024),
+                                nn.ReLU(),
+                                nn.Dropout(p=0.5),
+                                nn.Linear(1024,len(class_names)),
+                                nn.LogSoftmax(dim=1))
+
+###############################################################
