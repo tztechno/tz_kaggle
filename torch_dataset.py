c84c1f5d13d926710e06ee179b6d1734876babfe
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-23 18:47:19 +0900

    Update torch_dataset.py

--- torch_dataset.py
+++ torch_dataset.py
@@ -1,3 +1,33 @@
+######################################################
+
+import torch
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        path = self.dataframe.loc[index, 'path']
+        label = self.dataframe.loc[index, 'label']
+        # ここで画像を読み込んだり、前処理を行ったりする
+        # 例えば、PILを使用して画像を読み込む場合は以下のように書けます
+        # image = Image.open(path).convert('RGB')
+        # または、OpenCVを使用する場合は以下のように書けます
+        # image = cv2.imread(path)
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # 前処理を適用する場合は、以下のように書けます
+        # transform = transforms.Compose([
+        #     transforms.Resize((224, 224)),
+        #     transforms.ToTensor(),
+        #     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+        #                          std=[0.229, 0.224, 0.225])
+        # ])
+        # image = transform(image)
+        return path, label
 
 ######################################################
 #create dataset from df of path/label
@@ -16,7 +46,7 @@ class MyDataset(Dataset):
         path, label = self.data_df.iloc[idx]
         img = Image.open(os.path.join(dir0,path)).convert('RGB')
         img = img.resize((224, 224))
-        img = torch.tensor(np.array(img)).permute(2, 0, 1).float() / 255.0
+        img = torch.tensor(np.array(img)).permute(2,0,1).float() / 255.0
         
         return img, label
     
