22a02ca9cb7de6dcdfdb420fbc67e48876979fe0
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-25 12:20:50 +0900

    Update transoform.py

--- transoform.py
+++ transoform.py
@@ -8,3 +8,14 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 #######################################
+
+train_transform=transforms.Compose([
+        transforms.RandomRotation(10),      # rotate +/- 10 degrees
+        transforms.RandomHorizontalFlip(),  # reverse 50% of images
+        transforms.Resize(224),             # resize shortest side to 224 pixels
+        transforms.CenterCrop(224),         # crop longest side to 224 pixels at center
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+])
+#######################################
