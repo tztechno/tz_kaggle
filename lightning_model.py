4e606c1ab916ca2a6d88592611d1b5083f9aa505
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-25 17:35:28 +0900

    Create lightning_model.py

---
+++ lightning_model.py
@@ -0,0 +1,54 @@
+class ConvolutionalNetwork(LightningModule):
+    
+    def __init__(self):
+        super(ConvolutionalNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(16 * 54 * 54, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 20)
+        self.fc4 = nn.Linear(20, 4)
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 16 * 54 * 54)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = F.relu(self.fc3(X))
+        X = self.fc4(X)
+        return F.log_softmax(X, dim=1)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        X, y = train_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("train_loss", loss)
+        self.log("train_acc", acc)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        X, y = val_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("val_loss", loss)
+        self.log("val_acc", acc)
+
+    def test_step(self, val_batch, batch_idx):
+        X, y = val_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("val_loss", loss)
+        self.log("val_acc", acc)
