892fc25ba0dcdcd076eb88c00006082d2afd2e93
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-26 10:40:16 +0900

    Update class_names.py

--- class_names.py
+++ class_names.py
@@ -5,4 +5,8 @@ Name=data['Species'].unique().tolist()
 
 #################################
 
+dataset0=datasets.ImageFolder(root="/kaggle/input/flower-photos-by-the-tensorflow-team/flower_photos",transform=None)
+class_names=dataset0.classes
+
+#################################
 
