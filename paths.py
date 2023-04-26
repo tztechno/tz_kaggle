e58f601b729f1b58526dd132d6eeb99d95e8ff3d
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-26 10:34:10 +0900

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,4 +1,18 @@
+#################################################################  
+
 paths=[]
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         paths+=[(os.path.join(dirname, filename))]
+
+#################################################################      
+        
+path_label = []
+for dirname, _, filenames in os.walk('/kaggle/input/rockpaperscissors'):
+    for filename in filenames:
+        if filename[-4:] == '.png':
+            path = os.path.join(dirname, filename)
+            label = dirname.split('/')[-1]
+            path_label += [(path, normal_mapping[label])] 
+
+#################################################################      
