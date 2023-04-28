841fc43d1cdf9e7cd71aca84fea36e8550dc05e8
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-29 02:52:16 +0900

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -1,4 +1,14 @@
 
+##########################
+
+import pandas as pd
+import json
+
+with open('/kaggle/input/news-category-dataset/News_Category_Dataset_v3.json', 'r') as f:
+    data = f.read()
+
+data = [json.loads(line) for line in data.split('\n') if line]
+df = pd.DataFrame.from_records(data)
 
 ##########################
 
@@ -8,7 +18,6 @@ json_file_path = 'path/to/json/file.json'
 
 with open(json_file_path, 'r') as f:
     data = json.load(f)
-
 
 ##########################
 
