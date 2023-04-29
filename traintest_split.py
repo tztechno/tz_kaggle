62cded30dcb18c370661ce857d704e3deb7fcf39
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-29 16:58:21 +0900

    Update traintest_split.py

--- traintest_split.py
+++ traintest_split.py
@@ -1,3 +1,31 @@
+
+########################
+
+class DataModule(pl.LightningDataModule):
+    def __init__(self, data, test_size=0.2, random_state=42):
+        super().__init__()
+        self.data = data
+        self.test_size = test_size
+        self.random_state = random_state
+        
+    def setup(self, stage=None):
+        if stage == 'fit' or stage is None:
+            train_data, val_data = train_test_split(self.data, test_size=self.test_size, random_state=self.random_state)
+            self.train_data = DataSet(train_data)
+            self.val_data = DataSet(val_data)
+        
+        if stage == 'test' or stage is None:
+            self.test_data = DataSet(self.data)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, batch_size=32, shuffle=True, num_workers=2)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data, batch_size=32, num_workers=2)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_data, batch_size=32, num_workers=2)
+
 ########################
 
 from sklearn.model_selection import train_test_split
@@ -34,9 +62,7 @@ train_size = int(len(df) * 0.8)
 train_idx, test_idx = idx[:train_size], idx[train_size:]
 train_df, test_df = df.iloc[train_idx], df.iloc[test_idx]
 
-
 ########################
-
 
 m=len(data)
 M=list(range(m))
