7397ac9c6f272b1ef6389f5b894b8556a7e752b9
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-30 00:01:12 +0900

    Create clean_text.py

---
+++ clean_text.py
@@ -0,0 +1,14 @@
+import string
+from nltk.corpus import stopwords
+
+def clean_text(text):
+    '''Make text lowercase, remove text in square brackets,remove links,remove punctuation
+    and remove words containing numbers.'''
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
