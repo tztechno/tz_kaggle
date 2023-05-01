a45dbed2bc4ada65f832fca036efc4bf9d0b37f2
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-02 01:44:53 +0900

    Update torch_Net.py

--- torch_Net.py
+++ torch_Net.py
@@ -1,5 +1,19 @@
 
 ###############################################################
+class MyCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = models.alexnet(pretrained=True)
+        self.model.classifier = nn.Sequential( nn.Linear(9216,1024),
+                                             nn.ReLU(),
+                                             nn.Dropout(p=0.5),
+                                             nn.Linear(1024,5),
+                                             nn.LogSoftmax(dim=1))
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+###############################################################
 
 #(224,224)
 model= models.alexnet(pretrained=True)
