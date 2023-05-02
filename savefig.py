836a3c914e34bd6ea5b708b47687ad82a66b2471
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-02 12:51:25 +0900

    Create savefig.py

---
+++ savefig.py
@@ -0,0 +1,12 @@
+plt.imshow(image)
+plt.show()
+plt.savefig('plot0.png')
+
+show_anns(masks1)
+plt.show()
+plt.savefig('plot1.png')
+
+plt.imshow(image) 
+show_anns(masks1)
+plt.show()
+plt.savefig('plot2.png')
