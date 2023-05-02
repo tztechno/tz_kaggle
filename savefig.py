74e41a2bba13c0d4b06b33ba0d74bb52fb7be01b
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-02 13:53:02 +0900

    Update savefig.py

--- savefig.py
+++ savefig.py
@@ -1,12 +1,23 @@
-plt.imshow(image)
-plt.show()
-plt.savefig('plot0.png')
 
-show_anns(masks1)
+blank_img=np.zeros(image.shape)
+white_img=np.ones(image.shape)*255
+
+plt.imshow(image)
+plt.savefig('plot0.png')
 plt.show()
-plt.savefig('plot1.png')
+
+plt.imshow(blank_img)
+show_anns(masks1)
+plt.savefig('plot1b.png')
+plt.show()
+
+plt.imshow(white_img)
+show_anns(masks1)
+plt.savefig('plot1w.png')
+plt.show()
 
 plt.imshow(image) 
 show_anns(masks1)
-plt.show()
 plt.savefig('plot2.png')
+plt.show()
+
