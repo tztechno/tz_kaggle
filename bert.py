c8dc0949fe0a4516d72d6f6ab2faf84101637824
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-11 02:23:18 +0900

    Update bert.py

--- bert.py
+++ bert.py
@@ -11,6 +11,28 @@ encoded_input = tokenizer(text, return_tensors='pt')
 output = model(**encoded_input)
 
 #######################################################
+#DeBERTa in PyTorch:
+
+from transformers import DebertaTokenizer, DebertaModel
+
+tokenizer = DebertaTokenizer.from_pretrained('microsoft/deberta-base')
+model = DebertaModel.from_pretrained('microsoft/deberta-base')
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
+
+#######################################################
+#RoBERTa in PyTorch:
+
+from transformers import RobertaTokenizer, RobertaModel
+
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+model = RobertaModel.from_pretrained('roberta-base')
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
+
+#######################################################
 #BERT in TensorFlow:
 
 from transformers import BertTokenizer, TFBertModel
