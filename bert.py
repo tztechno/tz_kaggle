21ae9db06d7909eed47303d41df81607327ed105
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-11 01:25:17 +0900

    Update bert.py

--- bert.py
+++ bert.py
@@ -1,4 +1,6 @@
-######################################
+
+#######################################################
+#in PyTorch:
 
 from transformers import BertTokenizer, BertModel
 
@@ -8,4 +10,15 @@ text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='pt')
 output = model(**encoded_input)
 
-######################################
+#######################################################
+#in TensorFlow:
+
+from transformers import BertTokenizer, TFBertModel
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = TFBertModel.from_pretrained("bert-base-uncased")
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='tf')
+output = model(encoded_input)
+
+#######################################################
