633d4557d5aca4777191e84c5b2ba68d921fef51
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-11 02:12:44 +0900

    Update bert.py

--- bert.py
+++ bert.py
@@ -1,6 +1,6 @@
 
 #######################################################
-#in PyTorch:
+#BERT in PyTorch:
 
 from transformers import BertTokenizer, BertModel
 
@@ -11,12 +11,34 @@ encoded_input = tokenizer(text, return_tensors='pt')
 output = model(**encoded_input)
 
 #######################################################
-#in TensorFlow:
+#BERT in TensorFlow:
 
 from transformers import BertTokenizer, TFBertModel
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = TFBertModel.from_pretrained("bert-base-uncased")
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='tf')
+output = model(encoded_input)
+
+#######################################################
+#DeBERTa in TensorFlow:
+
+from transformers import DebertaTokenizer, TFDebertaModel
+
+tokenizer = DebertaTokenizer.from_pretrained('microsoft/deberta-base')
+model = TFDebertaModel.from_pretrained("microsoft/deberta-base")
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='tf')
+output = model(encoded_input)
+
+#######################################################
+#RoBERTa in TensorFlow:
+
+from transformers import RobertaTokenizer, TFRobertaModel
+
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+model = TFRobertaModel.from_pretrained("roberta-base")
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='tf')
 output = model(encoded_input)
