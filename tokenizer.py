f71367c18025147928d2648120d36747a7606ffd
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-12 22:24:57 +0900

    Create tokenizer.py

---
+++ tokenizer.py
@@ -0,0 +1,32 @@
+  
+#########################################################
+
+def create_data(text):
+    encoded = tokenizer.batch_encode_plus(
+        text,
+        add_special_tokens = True,
+        max_length= max_len,
+        padding='max_length',
+        truncation=True,
+        return_attention_mask=True)
+
+    input_ids       = np.array(encoded["input_ids"], dtype="int32")
+    attention_masks = np.array(encoded["attention_mask"], dtype="int32")
+
+    return {"input_ids": input_ids, "attention_masks": attention_masks}
+  
+#########################################################
+
+この関数は、与えられたtextをトークン化し、エンコードして、入力として使用できる形式に変換します。
+トークン化は、与えられたテキストを小さなトークンに分割するプロセスです。
+エンコードは、各トークンに対応する数値IDに変換するプロセスです。入力として使用できる形式に変換するために、
+input_idsとattention_masksという2つのNumPy配列が作成されます。
+
+input_idsは、エンコードされたトークンの数値IDの配列です。この配列は、モデルに入力するテキストデータの部分です。
+
+attention_masksは、モデルが注意を払うべきトークンを示すバイナリマスクの配列です。
+つまり、パディングされたトークン（テキストの短いシーケンスを最大長に拡張するために追加されたトークン）は、
+モデルが無視する必要があるため、0でマスクされます。一方、元のテキストに含まれるトークンは、1でマスクされます。
+  
+
+#########################################################  
