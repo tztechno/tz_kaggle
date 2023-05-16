9f2c4773f32b10460764e4890931864d0a6dfa1e
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-16 13:09:52 +0900

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,11 @@
+# Import the YouTubeVideo class from IPython.display
+from IPython.display import YouTubeVideo
+
+# Define the YouTube video ID
+video_id = 'aE8I7bDf62M'  # Replace YOUR_VIDEO_ID with the actual video ID
+
+# Create a YouTubeVideo object with the specified video ID
+video = YouTubeVideo(video_id)
+
+# Display the video
+display(video)
