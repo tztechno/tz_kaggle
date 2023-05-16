b9799eccc5a2b773588266448d9a33d1224642ca
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-16 18:59:47 +0900

    Create video_capture.py

---
+++ video_capture.py
@@ -0,0 +1,11 @@
+def VideoCapture(path):
+    cap = cv2.VideoCapture(path)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
