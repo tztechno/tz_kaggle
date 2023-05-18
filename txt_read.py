1074f14fec6aad700bc275c88827523f3fb57607
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-18 23:12:41 +0900

    Update txt_read.py

--- txt_read.py
+++ txt_read.py
@@ -1,3 +1,4 @@
+#################################
 
 file_path = "path/to/file.txt"
 
@@ -5,3 +6,16 @@ with open(file_path, 'r') as file:
     content = file.read()
 
 print(content)
+
+#################################
+
+from pathlib import Path
+
+file_path = Path("path/to/file.txt")
+
+with file_path.open('r') as file:
+    content = file.read()
+
+print(content)
+
+#################################
