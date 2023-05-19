dc5bee8eaa25a4e51742ee44c6bfdf1d4d62d630
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-19 12:47:36 +0900

    Update super_gradients.py

--- super_gradients.py
+++ super_gradients.py
@@ -24,3 +24,25 @@ from super_gradients.training.exceptions.sg_model_exceptions import UnsupportedO
 from super_gradients.common.object_names import Datasets
 from super_gradients.common.registry.registry import register_dataset
 from super_gradients.common.abstractions.abstract_logger import get_logger
+
+
+
+
+
+# onject detection
+
+url = "https://previews.123rf.com/images/freeograph/freeograph2011/freeograph201100150/158301822-group-of-friends-gathering-around-table-at-home.jpg"
+yolo_nas_l.predict(url, conf=0.25).show()
+
+# semantic segmentation
+
+
+# image classification
+
+
+
+
+
+
+
+
