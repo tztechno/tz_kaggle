044c56b2ccf7ea38856e60b901c77a94530865d4
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-21 11:39:32 +0900

    Update rle.py

--- rle.py
+++ rle.py
@@ -1,3 +1,5 @@
+##################################################################
+
 import numpy as np
 
 def rle(output):
@@ -20,3 +22,41 @@ def decode_RLE(rle_string, shape):
     img = img.reshape(shape).T
     return img
 
+##################################################################
+
+# these functions are given in https://www.kaggle.com/code/inversion/contrails-rle-submission
+
+def rle_encode(x, fg_val=1):    
+    dots = np.where(
+        x.T.flatten() == fg_val)[0]  # .T sets Fortran order down-then-right
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if b > prev + 1:
+            run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+
+
+def list_to_string(x):
+    if x: # non-empty list
+        s = str(x).replace("[", "").replace("]", "").replace(",", "")
+    else:
+        s = '-'
+    return s
+
+
+def rle_decode(mask_rle, shape=(256, 256)):
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    if mask_rle != '-': 
+        s = mask_rle.split()
+        starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+        starts -= 1
+        ends = starts + lengths
+        for lo, hi in zip(starts, ends):
+            img[lo:hi] = 1
+    return img.reshape(shape, order='F')  # Needed to align to RLE direction
+
+
+##################################################################
