ccb49d94995f306cb7a4542613d0abd1a687abee
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-26 11:56:44 +0900

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,4 +1,11 @@
+######################
+
 cp /path/to/source/file.txt /path/to/destination/directory/
 
+######################
+
+import shutil
+
+shutil.copy(source_file, destination_file)
 
 ######################
