b28d802ddd92a5a26103c03437542c1c82de1b4f
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-27 13:18:15 +0900

    Update track.py

--- track.py
+++ track.py
@@ -30,7 +30,7 @@ $ python examples/track.py --yolo-model yolov8n.pt      # bboxes only
                                         yolov8n-pose.pt # bboxes + pose estimation    
     
     
-$ python examples/track.py --tracking-method deepocsort
+$ python examples/track.py --tracking-method deepocsort  #default
                                              strongsort
                                              ocsort
                                              bytetrack
@@ -50,7 +50,8 @@ $ python examples/track.py --source 0 --yolo-model yolov8n.pt --img 640
                                                    yolov8l.onnx 
                                                    yolov8x.pt --img 1280
          
-$ python examples/track.py --source 0 --reid-model lmbn_n_cuhk03_d.pt
+$ python examples/track.py --source 0 --reid-model mobilenetv2_x1_4_dukemtmcreid.pt
+                                                   lmbn_n_cuhk03_d.pt
                                                    osnet_x0_25_market1501.pt
                                                    mobilenetv2_x1_4_msmt17.engine
                                                    resnet50_msmt17.onnx
