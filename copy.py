116e2c8bdc2886e4b188fd7272a103a5f1c6b629
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-27 19:01:54 +0900

    Update copy.py

--- copy.py
+++ copy.py
@@ -9,3 +9,17 @@ import shutil
 shutil.copy(source_file, destination_file)
 
 ######################
+
+import shutil
+
+def copy_images_to_folder(paths, destination_folder):
+    for path in paths:
+        shutil.copy2(path, destination_folder)
+        print(f"コピーが完了しました: {path}")
+        
+image_paths = ['/path/to/image1.jpg', '/path/to/image2.jpg', '/path/to/image3.jpg']
+destination_folder = '/path/to/destination_folder'
+
+copy_images_to_folder(image_paths, destination_folder)
+
+######################
