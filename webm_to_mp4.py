1c8a0ff850ab07aa8db281bc83aa61b29f5f99a3
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-28 17:10:22 +0900

    Update webm_to_mp4.py

--- webm_to_mp4.py
+++ webm_to_mp4.py
@@ -2,4 +2,4 @@ from moviepy.editor import *
 
 def convert_webm_to_mp4(webm_file, mp4_file):
     video = VideoFileClip(webm_file)
-    video.write_videofile(mp4_file, codec='libx264')
+    video.write_videofile(mp4_file, fps=30, codec='libx264')
