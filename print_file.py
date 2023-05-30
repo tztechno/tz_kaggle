5928bc7fd33193309a575936edf552b76f9c4a6e
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-30 13:04:21 +0900

    Update print_file.py

--- print_file.py
+++ print_file.py
@@ -1,8 +1,11 @@
 import subprocess
 
-def print_file(file_path):
-    subprocess.Popen(['lp', file_path])
+def print_files_in_folder(folder_path):
+    file_list = os.listdir(folder_path)
+    for file_name in file_list:
+        file_path = os.path.join(folder_path, file_name)
+        subprocess.Popen(['lp', file_path])
 
 # 使用例
-file_path = '/path/to/file.txt'
-print_file(file_path)
+folder_path = '/path/to/folder'
+print_files_in_folder(folder_path)
