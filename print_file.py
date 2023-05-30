fab07ef80c776134ea99529106e3f1c92f5da136
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-30 13:16:46 +0900

    Update print_file.py

--- print_file.py
+++ print_file.py
@@ -1,11 +1,10 @@
-import subprocess
+import os
 
-def print_files_in_folder(folder_path):
+def print_file_names_in_folder(folder_path):
     file_list = os.listdir(folder_path)
     for file_name in file_list:
-        file_path = os.path.join(folder_path, file_name)
-        subprocess.Popen(['lp', file_path])
+        print(file_name)
 
 # 使用例
 folder_path = '/path/to/folder'
-print_files_in_folder(folder_path)
+print_file_names_in_folder(folder_path)
