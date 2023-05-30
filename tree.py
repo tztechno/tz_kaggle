2b93eb79b258d7ee4dfe5ba98a55bd26b9e6e7a8
tztechno <47345279+tztechno@users.noreply.github.com>
2023-05-30 13:22:45 +0900

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,20 @@
+import os
+
+def tree(directory, level=0):
+    prefix = "|   " * level
+    prefix += "|-- "
+    
+    contents = os.listdir(directory)
+    
+    for item in sorted(contents):
+        item_path = os.path.join(directory, item)
+        
+        if os.path.isdir(item_path):
+            print(prefix + item + "/")
+            tree(item_path, level+1)
+        else:
+            print(prefix + item)
+
+# フォルダのパスを指定して実行する
+folder_path = "/path/to/folder"
+tree(folder_path)
