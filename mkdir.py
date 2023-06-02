94fd7ea381fc3951b548501dd1c80ecf68abff51
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-02 22:57:42 +0900

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -1,11 +1,13 @@
 ######################################
 
+os.makedirs('datasets', exist_ok=True)
+
+######################################
+
 import os
 
-# 新しいディレクトリ名を指定
 new_folder = "xxx"
 
-# 現在の作業ディレクトリに新しいディレクトリを作成
 if not os.path.exists(new_folder):
     os.makedirs(new_folder)
     
@@ -19,8 +21,6 @@ shutil.rmtree(directory)
 ######################################    
 
 rm -r kaggle/working/yolo_tracking
-
-#再帰的にフォルダ内のファイルやサブディレクトリも削除する
 
 ######################################
 
