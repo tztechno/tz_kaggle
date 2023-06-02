dfe986d92b4ad446d65911023e01272a2401e6c9
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-03 03:02:36 +0900

    Update zipfile.py

--- zipfile.py
+++ zipfile.py
@@ -1,13 +1,29 @@
 
+################################################################
+
 import zipfile
 
-# ファイル名と保存先ディレクトリを指定する
+def create_zip(source_folder, destination_zip):
+    with zipfile.ZipFile(destination_zip, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+        for root, dirs, files in os.walk(source_folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zip_ref.write(file_path, arcname=os.path.relpath(file_path, source_folder))
+
+def extract_zip(zip_file, destination_folder):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(destination_folder)
+
+################################################################
+
+import zipfile
+
 file_name = 'model.pt'
 save_dir = './'
 
-# Zipファイル名を指定する
 zip_name = 'submission.zip'
 
-# Zipファイルを作成する
 with zipfile.ZipFile(zip_name, mode='w') as zipf:
     zipf.write(file_name, arcname=file_name)
+
+################################################################
