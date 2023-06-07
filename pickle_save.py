9f6ed3ff4547855fe5b3bdcd0e25db239482cd2f
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-08 00:53:48 +0900

    Update pickle_save.py

--- pickle_save.py
+++ pickle_save.py
@@ -1,4 +1,24 @@
 ####################################  
+#first time
+#clf = RandomForestRegressor()
+
+#second time and after
+with open('/kaggle/input/iris-species-random-forest-repeated/clf.pkl', 'rb') as f:
+    clf = pickle.load(f)
+ss = ShuffleSplit(n_splits=5,train_size=0.7,test_size =0.3,random_state=25) 
+
+X=trainX.values
+y=trainY.values
+
+for train_index, test_index in ss.split(X):
+    trainx, testx = X[train_index], X[test_index]
+    trainy, testy = y[train_index], y[test_index]
+    clf.fit(trainx,trainy) 
+    print(clf.score(testx, testy))
+    
+with open('clf.pkl', 'wb') as f:
+    pickle.dump(clf, f)    
+####################################  
 
 import pickle
 import lightgbm as lgbm
