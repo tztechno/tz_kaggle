9a137528911305bde819f09fc183ef03a6b082b2
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-08 23:24:16 +0900

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -1,29 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# データポイントの座標を生成
 x = np.random.randn(1000)
 y = np.random.randn(1000)
 
-# ヒストグラムを計算
 heatmap, xedges, yedges = np.histogram2d(x, y, bins=50)
 
-# プロット領域を作成
 fig, ax = plt.subplots()
 
-# ヒートマップを描画
 im = ax.imshow(heatmap.T, cmap='hot', origin='lower', extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
 
-# カラーバーを追加
-cbar = fig.colorbar(im)
+#'hot': 赤から黄色までのホットなカラーマップです。高い値がより明るい色で表示されます。
+#'cool': 青からシアンまでのクールなカラーマップです。低い値がより明るい色で表示されます。
+#'viridis': カラーマップの感じに基づいた色のマッピングです。低い値から高い値に徐々に変化し、黄緑から深い青までの範囲をカバーします。
+#'magma': 黒から赤紫までのマグマのようなカラーマップです。低い値から高い値に徐々に変化し、暗い色から明るい色までを含みます。
+#'inferno': 黒から黄色までのインフェルノのようなカラーマップです。低い値から高い値に徐々に変化し、暗い色から明るい色までを含みます。
 
-# ラベルやタイトルなどのカスタマイズ
+cbar = fig.colorbar(im)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_title('Heat Map')
-
-# グリッドを表示
 ax.grid(True)
-
-# プロットを表示
 plt.show()
