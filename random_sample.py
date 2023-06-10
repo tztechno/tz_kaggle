2c3ef678818cfc3ba9544db3e31a8cfcc2d6cca4
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-10 18:17:11 +0900

    Update random_sample.py

--- random_sample.py
+++ random_sample.py
@@ -7,3 +7,21 @@ M=random.sample(N,3000)
 data_df4=data_df3.iloc[M].reset_index(drop=True)
 
 ##############################
+
+import random
+
+def weighted_random_choice(choices, weights):
+    total = sum(weights)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in zip(choices, weights):
+        if upto + w >= r:
+            return c
+        upto += w
+
+choices = [1, 2, 3, 4, 5, 6]
+weights = [1, 1, 1, 2, 2, 2]  # 大きい数字の重みを増やす
+
+rating = weighted_random_choice(choices, weights)
+
+##############################
