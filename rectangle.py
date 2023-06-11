b24145c344011e372d486db40fa40f5fb9c0accc
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-11 13:36:44 +0900

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,35 @@
+####################################################
+
+def draw_box2(n0):
+    
+    ipath=ppaths[n0]
+    image=cv2.imread(ipath)
+    H,W=image.shape[0],image.shape[1]
+    file=ipath.split('/')[-1]
+    
+    if PBOX[PBOX['file']==file] is not None:
+        box=PBOX[PBOX['file']==file]
+        box=box.reset_index(drop=True)
+        #display(box)
+
+        for i in range(len(box)):
+            label=box.loc[i,'class']
+            x=int(box.loc[i,'x'])
+            y=int(box.loc[i,'y'])
+            x2=int(box.loc[i,'x2']) 
+            y2=int(box.loc[i,'y2'])
+            #print(label,x,y,x2,y2)
+            cv2.putText(image, f'{label}', (x, int(y-0.5)), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+            cv2.rectangle(image,(x,y),(x2,y2),(0,255,0),5) #green
+    
+    #plt.imshow(image)
+    #plt.show()   
+    
+    return image
+
+####################################################
+
 def draw_box2(n0):
     
     ipath=ppaths[n0]
@@ -25,3 +57,5 @@ def draw_box2(n0):
     #plt.show()   
     
     return image
+
+####################################################
