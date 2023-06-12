6d547e66b9dc162a63116a0b0ab60ed96477fe83
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-12 11:43:12 +0900

    Update plt_show.py

--- plt_show.py
+++ plt_show.py
@@ -13,3 +13,30 @@ ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.show()
 
 #################################
+
+plt.figure(figsize=(6,6))
+plt.imshow(sketch, cmap='gray')
+plt.axis('off') 
+plt.tight_layout()
+plt.show()
+
+#################################
+
+plt.figure(figsize=(6,6))
+plt.imshow(image)
+plt.axis('off') 
+plt.show()
+
+plt.figure(figsize=(6,6))
+plt.imshow(sketch, cmap='gray')
+plt.axis('off') 
+plt.show()
+
+fig, axs = plt.subplots(figsize=(6,6))
+axs.imshow(image) 
+axs.imshow(sketch, cmap='gray', alpha=0.6) #0.1--0.9
+axs.axis('off')
+plt.show()
+fig.savefig('save.png', bbox_inches='tight')
+
+#################################
