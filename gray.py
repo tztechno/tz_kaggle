03e09351f91372a3396fa8105c4fd8e895d17fab
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-14 12:01:34 +0900

    Update gray.py

--- gray.py
+++ gray.py
@@ -1,22 +1,23 @@
 ########################################################
 
+import cv2
 
+def convert_to_gray(image_path):
+    image = cv2.imread(image_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("gray_image.jpg", gray_image)
+    
+convert_to_gray("input_image.jpg")
 
 ########################################################
+
 from PIL import Image
 
 def convert_to_gray(image_path):
-    # 画像を開く
     image = Image.open(image_path)
-    
-    # グレースケールに変換
     gray_image = image.convert("L")
-    
-    # グレースケール画像を保存
     gray_image.save("gray_image.jpg")
     
-    print("グレースケール画像を保存しました。")
-
-# 画像のパスを指定して呼び出し
 convert_to_gray("input_image.jpg")
+
 ########################################################
