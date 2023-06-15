7386bba2762b0ba2393b651d3eb6c9d9bb358a1c
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-16 02:07:42 +0900

    Update zipfile.py

--- zipfile.py
+++ zipfile.py
@@ -34,3 +34,22 @@ with zipfile.ZipFile(zip_name, mode='w') as zipf:
     zipf.write(file_name, arcname=file_name)
 
 ################################################################
+
+import zipfile
+import os
+
+def zip_folder(folder_path, zip_path):
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, folder_path))
+
+# 使用例
+folder_to_zip = '/path/to/folder'  # ZIP化したいフォルダのパス
+zip_file = '/path/to/archive.zip'  # 作成するZIPファイルのパス
+
+zip_folder(folder_to_zip, zip_file)
+
+
+################################################################
