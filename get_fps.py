2aab7518e1c9b3a7d3cb61c6e1d8edff4d5c6a1c
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-15 12:33:35 +0900

    Update get_fps.py

--- get_fps.py
+++ get_fps.py
@@ -1,3 +1,5 @@
+##############################################
+
 from moviepy.editor import VideoFileClip
 
 def get_fps(mp4_file):
@@ -9,3 +11,24 @@ def get_fps(mp4_file):
 mp4_file_path = "path/to/your/video.mp4"
 fps = get_fps(mp4_file_path)
 print("FPS:", fps)
+
+##############################################
+
+from moviepy.editor import VideoFileClip
+
+def get_mp4_info(filename):
+    clip = VideoFileClip(filename)
+    duration = clip.duration
+    frame_count = clip.reader.nframes
+    fps = clip.reader.fps
+    clip.close()
+    return duration, frame_count, fps
+
+# 使用例
+filename = "example.mp4"
+duration, frame_count, fps = get_mp4_info(filename)
+print("長さ（秒）:", duration)
+print("フレーム数:", frame_count)
+print("FPS:", fps)
+
+##############################################
