cc364cc43d12f8d6456c89fbe7ce38953cad756b
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-17 14:07:19 +0900

    Update kaggle_wandb.py

--- kaggle_wandb.py
+++ kaggle_wandb.py
@@ -15,3 +15,11 @@ except:
     anonymous = 'must'
     # Login to wandb anonymously and relogin if needed
     wandb.login(anonymous=anonymous, relogin=True)
+
+#######################################################  
+
+from kaggle_secrets import UserSecretsClient
+import wandb
+user_secrets = UserSecretsClient()
+secret_value = user_secrets.get_secret("wandb_api_key")
+wandb.login(key=secret_value)
