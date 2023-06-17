1fcae40761a5021cdc742254e49c28d16fad2aed
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-17 14:08:45 +0900

    Update wandb.py

--- wandb.py
+++ wandb.py
@@ -1,6 +1,8 @@
+
 # Wandb login:
 from kaggle_secrets import UserSecretsClient
 import wandb
 user_secrets = UserSecretsClient()
 secret_value = user_secrets.get_secret("wandb_api_key")
 wandb.login(key=secret_value)
+
