47e9ebc3fc98f78531fa5d39f1c30ffc754c9b8f
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-18 19:42:15 +0900

    Update clorama.py

--- clorama.py
+++ clorama.py
@@ -3,3 +3,6 @@ blk = Style.BRIGHT + Fore.BLACK
 red = Style.BRIGHT + Fore.RED
 blu = Style.BRIGHT + Fore.BLUE
 res = Style.RESET_ALL
+
+blk,red,blk
+('\x1b[1m\x1b[30m', '\x1b[1m\x1b[31m', '\x1b[1m\x1b[30m')
