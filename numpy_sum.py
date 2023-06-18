e80e38b67e23c6afba0b308af1c71cc1ae6fad59
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-18 19:04:21 +0900

    Update numpy_sum.py

--- numpy_sum.py
+++ numpy_sum.py
@@ -7,9 +7,13 @@ arrays = [np.random.rand(1, 512, 512, 3) for _ in range(n)]
 result = np.sum(arrays, axis=0)
 print(result.shape)  # (1, 512, 512, 3)
 
-
 #################################
 
 MASKS = np.vstack([MASKS, np.array(masks)])
+
+#################################
+
+img_ex = np.expand_dims(img, axis=0)
+MASKS = np.vstack([MASKS, img_ex])
 
 #################################
