882f24fe0c00e1b33559a015c7fd77b29778d0a1
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-19 18:00:26 +0900

    Update torch_transform.py

--- torch_transform.py
+++ torch_transform.py
@@ -1,3 +1,4 @@
+#####################################
 
 transform = transforms.Compose([
     transforms.Resize(256),
@@ -5,3 +6,17 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
+
+#####################################
+
+transform=transforms.Compose([
+        transforms.RandomRotation(10),      # rotate +/- 10 degrees
+        transforms.RandomHorizontalFlip(),  # reverse 50% of images
+        transforms.Resize(224),             # resize shortest side to 224 pixels
+        transforms.CenterCrop(224),         # crop longest side to 224 pixels at center
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+])
+
+#####################################
