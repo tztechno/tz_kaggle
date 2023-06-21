54a22fcde8ab4f312935317e625d0aa258bd0227
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-21 12:10:01 +0900

    Update PIL_image_show.py

--- PIL_image_show.py
+++ PIL_image_show.py
@@ -1,6 +1,21 @@
+############################################
 
 from PIL import Image
 
 image_path = 'path/to/image.jpg'
 img = Image.open(image_path)
 img.show()
+
+############################################
+
+import numpy as np
+from PIL import Image
+
+np_image = np.array([[255, 0, 0],
+                     [0, 255, 0],
+                     [0, 0, 255]], dtype=np.uint8)
+
+pil_image = Image.fromarray(np_image)
+pil_image.show()
+
+############################################
