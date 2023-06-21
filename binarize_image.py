320a678f785ca7c48f66fbfc56d676bdb088d887
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-21 12:02:50 +0900

    Update binarize_image.py

--- binarize_image.py
+++ binarize_image.py
@@ -1,5 +1,28 @@
+####################################################################
+
 def binarize_image(image, threshold):
     _, binary_image = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
     return binary_image
 
 threshold_value = 127
+
+####################################################################
+
+import torchvision.transforms.functional as F
+from PIL import Image
+
+def binarize(image, threshold):
+    """
+    Args:
+        image (PIL.Image): 二値化する画像。
+        threshold (float): 二値化の閾値。
+    Returns:
+        PIL.Image: 二値化された画像。
+    """
+    image = image.convert("L")  # 画像をグレースケールに変換
+    image = F.to_tensor(image)  # テンソルに変換
+    image = (image > threshold).float()  # 閾値を用いて二値化
+    image = F.to_pil_image(image)  # PIL画像に変換
+    return image
+
+####################################################################
