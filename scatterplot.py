6c039a3ebdc655cb6a46cbeac33ecbb3a56ce2b9
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-23 13:37:18 +0900

    Update scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -1,3 +1,4 @@
+#############################################################################
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -9,4 +10,18 @@ for i in range(len(results)):
     ax.set_ylim([-700, 0])
     plt.savefig('./sample3/fig_'+str(i).zfill(5)+'.png')
     
-    
+#############################################################################
+
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+fig=make_subplots(specs=[[{"secondary_y":False}]])
+fig.add_trace(go.Scatter(x=data1['date'][682:],y=data1['positives mean'][682:],name='positives mean'),secondary_y=False,)
+fig.update_layout(autosize=False,width=700,height=500,title_text="Examined Positives (rolling 7-day) in Tokyo")
+fig.update_xaxes(title_text="Date")
+fig.update_yaxes(title_text="Cases",secondary_y=False)
+fig.show()
+
+#############################################################################
