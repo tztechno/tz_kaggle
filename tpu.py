a059a53c3c778dc1cc5bbc2a9c4c6a0dccf612f2
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-24 17:29:47 +0900

    Create tpu.py

---
+++ tpu.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
