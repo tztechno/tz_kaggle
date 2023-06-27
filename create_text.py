195d31e0aad447d52dcf9a3cb37ddcc0f5ce2f66
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-27 21:42:55 +0900

    Update create_text.py

--- create_text.py
+++ create_text.py
@@ -23,3 +23,20 @@ with open(file_path, 'w') as file:
     file.write(content)
 
 ######################################################
+
+def write_list_to_file(lst, filename):
+    # ファイルを書き込みモードで開く
+    with open(filename, 'w') as file:
+        # リストの要素をスペース区切りで書き込む
+        file.write(' '.join(str(item) for item in lst))
+
+# テスト用のリスト
+my_list = [1, 2, 3, 4, 5]
+
+# ファイル名として使用する文字列
+file_name = "output.txt"
+
+# リストをファイルに書き込む
+write_list_to_file(my_list, file_name)
+
+######################################################
