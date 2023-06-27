4a4dad1d93105fa2da499c3fe040a97ce99d1036
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-27 11:47:24 +0900

    Update create_text.py

--- create_text.py
+++ create_text.py
@@ -1,12 +1,25 @@
+######################################################
+
+content = '''
+loguru>=0.7.0
+PyYAML>=5.3.1                  
+gdown>=4.7.1            
+GitPython>=3.1.0        
+filterpy>=1.4.5         
+'''
+file_path = 'requirements.txt'
+with open(file_path, 'w') as file:
+    file.write(content)
+
+######################################################
+
 content = '''
 This is line 1.
 This is line 2.
 This is line 3.
 '''
-
 file_path = 'example.txt'
-
 with open(file_path, 'w') as file:
     file.write(content)
 
-print(f"テキストファイル '{file_path}' が生成されました。")
+######################################################
