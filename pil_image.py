d4dcb330c03f32899968dc9a7235d27d31307a02
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-28 09:56:07 +0900

    Update pil_image.py

--- pil_image.py
+++ pil_image.py
@@ -2,3 +2,17 @@ from PIL import Image
 
 image = Image.open(img_path)
 image.show()
+
+########################################
+
+from PIL import Image, ImageDraw
+
+image = Image.open("画像ファイルのパス")
+
+draw = ImageDraw.Draw(image)
+
+x1, y1 = 100, 100  # 矩形の左上の座標
+x2, y2 = 200, 200  # 矩形の右下の座標
+
+draw.rectangle([(x1, y1), (x2, y2)], outline="red", width=2)
+image.show()
