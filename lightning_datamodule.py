1fb3d555e6e7dacc6c0928b958848ee29699f46e
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-30 16:51:18 +0900

    Update lightning_datamodule.py

--- lightning_datamodule.py
+++ lightning_datamodule.py
@@ -71,3 +71,30 @@ class DataModule(pl.LightningDataModule):
 
     
 ###################################################   
+
+class DataModule(pl.LightningDataModule):
+    
+    def __init__(self, transform=transform, batch_size=32):
+        super().__init__()
+        self.root_dir = "/kaggle/working/datasets"
+        self.transform = transform
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        dataset = datasets.ImageFolder(root=self.root_dir, transform=self.transform)
+        n_data = len(dataset)
+        n_train = int(0.8 * n_data)
+        n_test = n_data - n_train
+
+        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [n_train, n_test])
+
+        self.train_dataset = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        self.test_dataset = DataLoader(test_dataset, batch_size=self.batch_size)
+
+    def train_dataloader(self):
+        return self.train_dataset
+
+    def test_dataloader(self):
+        return self.test_dataset
+
+###################################################   
