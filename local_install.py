5ef15ed61f359abae4763ee9e7e0802e00e5123f
tztechno <47345279+tztechno@users.noreply.github.com>
2023-07-02 10:06:14 +0900

    Create local_install.py

---
+++ local_install.py
@@ -0,0 +1,7 @@
+
+#######################################################
+
+!cp -r /kaggle/input/imantics/ /kaggle/working/imantics
+!pip install /kaggle/working/imantics/imantics-0.1.12  --no-index --find-links=/kaggle/working/imantics/ 
+
+#######################################################
