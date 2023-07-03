3ad9ffa2638d4affb0ca98d73eb087b5be19b86d
tztechno <47345279+tztechno@users.noreply.github.com>
2023-07-03 14:33:03 +0900

    Update yolov8_track.py

--- yolov8_track.py
+++ yolov8_track.py
@@ -1,9 +1,7 @@
 
 !yolo mode=track model=yolov8n.pt source={input_video_path} 
 
-
-
 !yolo mode=track tracker="botsort.yaml" model=yolov8n.pt conf=0.25 source={input_video_path} save=True 
 
-
+!yolo mode=track tracker="bytetrack.yaml" model=yolov8n.pt conf=0.25 source={input_video_path} save=True 
 
