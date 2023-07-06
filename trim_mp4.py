63baeb1a040c815036e2c23898d6bbdecfec0c6e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-06 11:35:46 +0900

    Update trim_mp4.py

--- trim_mp4.py
+++ trim_mp4.py
@@ -1,4 +1,22 @@
+######################################################################################
+import subprocess
+
+def trim_video(input_file, output_file, start_time, end_time):
+    command = ['ffmpeg', '-i', input_file, '-ss', str(start_time), '-to', str(end_time), '-c', 'copy', output_file]
+    subprocess.run(command)
+
+input_file = '/kaggle/input/road-traffic-video-monitoring/traffic_video.avi'
+output_file = '/kaggle/working/yolov8_tracking/sample.mp4'
+start_time = 3
+end_time = 18
+
+trim_video(input_file, output_file, start_time, end_time)
+
+######################################################################################
+
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 def trim_video(input_file, output_file, start_time, end_time):
     ffmpeg_extract_subclip(input_file, start_time, end_time, targetname=output_file)
+
+######################################################################################
