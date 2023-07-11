b8face211985f48a3fe0a6482d081db2730b1857
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-11 17:52:34 +0900

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,2 +1,7 @@
-# 'date'列をdatetime64型に変換
-df['date'] = pd.to_datetime(df['date'])
+import pandas as pd
+
+# 'date'列を日付型に変換
+train2['date'] = pd.to_datetime(train2['date'])
+
+# 'date'列の型を確認
+print(train2['date'].dtype)
