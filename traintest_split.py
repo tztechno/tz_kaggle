ddcfdfe16041963c15d55298e2b59b58edc96680
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-15 13:17:00 +0900

    Update traintest_split.py

--- traintest_split.py
+++ traintest_split.py
@@ -1,3 +1,7 @@
+########################
+
+from sklearn.model_selection import train_test_split
+trainx, validx, trainy, validy = train_test_split(trainX, trainY, test_size=0.2, random_state=42)
 
 ########################
 
