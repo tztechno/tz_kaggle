9a0f68484959cd6de38ad1ac9d7764f361e096db
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-16 18:42:13 +0900

    Create unet.py

---
+++ unet.py
@@ -0,0 +1,198 @@
+#################################
+
+#UNET
+
+def conv_block(input, num_filters):
+    conv = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(input)
+    conv = tf.keras.layers.BatchNormalization()(conv)
+    conv = tf.keras.layers.Activation("relu")(conv)
+    conv = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(conv)
+    conv = tf.keras.layers.BatchNormalization()(conv)
+    conv = tf.keras.layers.Activation("relu")(conv)
+    return conv
+
+def encoder_block(input, num_filters):
+    skip = conv_block(input, num_filters)
+    pool = tf.keras.layers.MaxPool2D((2, 2))(skip)
+    return skip, pool
+
+def decoder_block(input, skip, num_filters):
+    up_conv = tf.keras.layers.Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    conv = tf.keras.layers.Concatenate()([up_conv, skip])
+    conv = conv_block(conv, num_filters)
+    return conv
+
+def Unet(input_shape):
+    inputs = tf.keras.layers.Input(input_shape)
+
+    skip1, pool1 = encoder_block(inputs, 64)
+    skip2, pool2 = encoder_block(pool1, 128)
+    skip3, pool3 = encoder_block(pool2, 256)
+    skip4, pool4 = encoder_block(pool3, 512)
+
+    bridge = conv_block(pool4, 1024)
+
+    decode1 = decoder_block(bridge, skip4, 512)
+    decode2 = decoder_block(decode1, skip3, 256)
+    decode3 = decoder_block(decode2, skip2, 128)
+    decode4 = decoder_block(decode3, skip1, 64)
+
+    outputs = tf.keras.layers.Conv2D(1, 1, padding="same", activation="sigmoid")(decode4)
+
+    model = tf.keras.models.Model(inputs, outputs, name="U-Net")
+    return model
+
+unet_model = Unet((512, 512, 3))
+unet_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+unet_model.summary()
+
+#################################
+
+#U2NET
+
+def conv_block(shape, out_ch, rate=1):
+    x = tf.keras.layers.Conv2D(out_ch, 3, padding="same", dilation_rate=1)(shape)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+    return x
+
+def RSU_L(shape, out_ch, M_ch, num_layers, rate=2):
+    x = conv_block(shape, out_ch)
+    inp_ch = x
+
+    skip_features = []
+    x = conv_block(x, M_ch)
+    skip_features.append(x)
+
+    for i in range(num_layers-2):
+        x = tf.keras.layers.MaxPool2D((2, 2))(x)
+        x = conv_block(x, M_ch)
+        skip_features.append(x)
+        
+    x = conv_block(x, M_ch, rate=rate)
+    skip_features.reverse()
+    x = tf.keras.layers.Concatenate()([x, skip_features[0]])
+    x = conv_block(x, M_ch)
+
+    for i in range(num_layers-3):
+        x = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(x)
+        x = tf.keras.layers.Concatenate()([x, skip_features[i+1]])
+        x = conv_block(x, M_ch)
+
+    x = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(x)
+    x = tf.keras.layers.Concatenate()([x, skip_features[-1]])
+    x = conv_block(x, out_ch)
+
+    x = tf.keras.layers.Add()([x, inp_ch])
+    return x
+
+def RSU_4F(shape, out_ch, M_ch):
+    x0 = conv_block(shape, out_ch, rate=1)
+
+    x1 = conv_block(x0, M_ch, rate=1)
+    x2 = conv_block(x1, M_ch, rate=2)
+    x3 = conv_block(x2, M_ch, rate=4)
+
+    x4 = conv_block(x3, M_ch, rate=8)
+
+    x = tf.keras.layers.Concatenate()([x4, x3])
+    x = conv_block(x, M_ch, rate=4)
+
+    x = tf.keras.layers.Concatenate()([x, x2])
+    x = conv_block(x, M_ch, rate=2)
+
+    x = tf.keras.layers.Concatenate()([x, x1])
+    x = conv_block(x, out_ch, rate=1)
+
+    x = tf.keras.layers.Add()([x, x0])
+    return x
+
+def u2net(shape, out_ch, M_ch, num_classes=1):
+    inputs = tf.keras.layers.Input(shape)
+
+    e1 = RSU_L(inputs, out_ch[0], M_ch[0], 7)
+    p1 = tf.keras.layers.MaxPool2D((2, 2))(e1)
+
+    e2 = RSU_L(p1, out_ch[1], M_ch[1], 6)
+    p2 = tf.keras.layers.MaxPool2D((2, 2))(e2)
+
+    e3 = RSU_L(p2, out_ch[2], M_ch[2], 5)
+    p3 = tf.keras.layers.MaxPool2D((2, 2))(e3)
+
+    e4 = RSU_L(p3, out_ch[3], M_ch[3], 4)
+    p4 = tf.keras.layers.MaxPool2D((2, 2))(e4)
+
+    e5 = RSU_4F(p4, out_ch[4], M_ch[4])
+    p5 = tf.keras.layers.MaxPool2D((2, 2))(e5)
+
+    b1 = RSU_4F(p5, out_ch[5], M_ch[5])
+    b2 = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(b1)
+
+    d1 = tf.keras.layers.Concatenate()([b2, e5])
+    d1 = RSU_4F(d1, out_ch[6], M_ch[6])
+    u1 = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(d1)
+
+    d2 = tf.keras.layers.Concatenate()([u1, e4])
+    d2 = RSU_L(d2, out_ch[7], M_ch[7], 4)
+    u2 = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(d2)
+
+    d3 = tf.keras.layers.Concatenate()([u2, e3])
+    d3 = RSU_L(d3, out_ch[8], M_ch[8], 5)
+    u3 = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(d3)
+
+    d4 = tf.keras.layers.Concatenate()([u3, e2])
+    d4 = RSU_L(d4, out_ch[9], M_ch[9], 6)
+    u4 = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(d4)
+
+    d5 = tf.keras.layers.Concatenate()([u4, e1])
+    d5 = RSU_L(d5, out_ch[10], M_ch[10], 7)
+
+    
+    y1 = tf.keras.layers.Conv2D(num_classes, 3, padding="same")(d5)
+
+    y2 = tf.keras.layers.Conv2D(num_classes, 3, padding="same")(d4)
+    y2 = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(y2)
+
+    y3 = tf.keras.layers.Conv2D(num_classes, 3, padding="same")(d3)
+    y3 = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation="bilinear")(y3)
+
+    y4 = tf.keras.layers.Conv2D(num_classes, 3, padding="same")(d2)
+    y4 = tf.keras.layers.UpSampling2D(size=(8, 8), interpolation="bilinear")(y4)
+
+    y5 = tf.keras.layers.Conv2D(num_classes, 3, padding="same")(d1)
+    y5 = tf.keras.layers.UpSampling2D(size=(16, 16), interpolation="bilinear")(y5)
+
+    y6 = tf.keras.layers.Conv2D(num_classes, 3, padding="same")(b1)
+    y6 = tf.keras.layers.UpSampling2D(size=(32, 32), interpolation="bilinear")(y6)
+
+    y0 = tf.keras.layers.Concatenate()([y1, y2, y3, y4, y5, y6])
+    y0 = tf.keras.layers.Conv2D(num_classes, 3, padding="same")(y0)
+
+    y0 = tf.keras.layers.Activation("sigmoid")(y0)
+    y1 = tf.keras.layers.Activation("sigmoid")(y1)
+    y2 = tf.keras.layers.Activation("sigmoid")(y2)
+    y3 = tf.keras.layers.Activation("sigmoid")(y3)
+    y4 = tf.keras.layers.Activation("sigmoid")(y4)
+    y5 = tf.keras.layers.Activation("sigmoid")(y5)
+    y6 = tf.keras.layers.Activation("sigmoid")(y6)
+
+    model = tf.keras.models.Model(inputs, outputs=y0, name="U2-Net")
+    return model
+
+def build_u2net(input_shape, num_classes=1):
+    out_ch = [64, 128, 256, 512, 512, 512, 512, 256, 128, 64, 64]
+    M_ch = [32, 32, 64, 128, 256, 256, 256, 128, 64, 32, 16]
+    model = u2net(input_shape, out_ch, M_ch, num_classes=num_classes)
+    return model
+
+def build_u2net_lite(input_shape, num_classes=1):
+    out_ch = [64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64]
+    M_ch = [16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16]
+    model = u2net(input_shape, out_ch, M_ch, num_classes=num_classes)
+    return model
+
+u2net_model = build_u2net((image_size, image_size, 3))
+u2net_model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
+u2net_model.summary()
+
+#################################
