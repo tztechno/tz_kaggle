99e527384248464d79cba8cb92fa62886f4de8b3
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-16 11:55:42 +0900

    Update one_hot_encoding.py

--- one_hot_encoding.py
+++ one_hot_encoding.py
@@ -24,3 +24,32 @@ dataY0=np.array(dataY0).astype(np.int64)
 dataY = torch.eye(245)[dataY0]
 
 ###########################
+
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import MultiLabelBinarizer
+
+multilabel_cols = ["genres", ]#"producers", "licensors", "studios"
+multilabel_dfs = []
+n_components=10
+for c in multilabel_cols:
+    list_srs = anime_df[c].map(lambda x: x.split(", ")).tolist()
+
+    mlb = MultiLabelBinarizer()
+    ohe_srs = mlb.fit_transform(list_srs)
+    if c == "genres" or c == "licensors":
+        # ユニーク数が多くないのでOne-hot表現のまま
+        col_df = pd.DataFrame(ohe_srs, columns=[f"ohe_{c}_{name}" for name in mlb.classes_])
+    else:
+        # ユニーク数が多いので、SVDで次元圧縮する
+        svd = TruncatedSVD(n_components=10)
+        svd_arr = svd.fit_transform(ohe_srs)
+        col_df = pd.DataFrame(
+            svd_arr,
+            columns=[f"svd_{c}_{ix}" for ix in range(n_components)]
+        )
+    multilabel_dfs.append(col_df)
+
+multilabel_df = pd.concat(multilabel_dfs, axis=1)
+display(multilabel_df[0:10].T)
+
+###########################
