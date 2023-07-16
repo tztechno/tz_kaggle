ed182c9533a96f6190111b20b307c7ab0264a61f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-17 01:05:40 +0900

    Create html.py

---
+++ html.py
@@ -0,0 +1,12 @@
+
+########################
+
+<p style="font-family:JetBrains Mono; font-weight:bold; letter-spacing: 2px; 
+color:#006600; font-size:140%; text-align:left;padding: 0px; 
+border-bottom: 3px solid #003300"> xxxxxx </p>
+
+########################
+########################
+########################
+########################
+########################
