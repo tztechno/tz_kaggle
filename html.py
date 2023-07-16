1ae9cabcffdc4c502968e1f520487e2f49420fc1
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-17 01:21:03 +0900

    Update html.py

--- html.py
+++ html.py
@@ -6,6 +6,11 @@ color:#006600; font-size:140%; text-align:left;padding: 0px;
 border-bottom: 3px solid #003300"> xxxxxx </p>
 
 ########################
+
+<div style="border-radius:10px; border:#DEB887 solid; padding: 15px; 
+background-color: #FFFAF0; font-size:100%; text-align:left">
+<h3 align="left"><font color='#DEB887'> xxxxx </font></h3>
+
 ########################
 ########################
 ########################
