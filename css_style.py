8824007f5cfbe002e6380392acaaa8353d45915e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-17 11:26:18 +0900

    Update css_style.py

--- css_style.py
+++ css_style.py
@@ -13,27 +13,18 @@ https://github.com/JoseCaliz/dotfiles/blob/main/css/vs_gruvbox.css
 !wget https://raw.githubusercontent.com/JoseCaliz/dotfiles/main/css/vs_monokai.css -O CSS.css -q
 !wget https://raw.githubusercontent.com/JoseCaliz/dotfiles/main/css/vs_gruvbox.css -O CSS.css -q  
 
-
-!wget http://bit.ly/3ZLyF82 -O CSS.css -q
-!wget https://raw.githubusercontent.com/JoseCaliz/dotfiles/main/css/vs_gruvbox.css -O CSS.css -q   
 ################################
 
-!wget https://raw.githubusercontent.com/JoseCaliz/dotfiles/main/css/vs_monokai.css -O CSS.css -q
-    
-from IPython.core.display import HTML
-with open('./gruvbox.css', 'r') as file:
-    custom_css = file.read()
-
-HTML(custom_css)
-
+!wget https://raw.githubusercontent.com/JoseCaliz/dotfiles/main/css/vs_gruvbox.css -O CSS.css -q  
+!wget http://bit.ly/3ZLyF82 -O CSS.css -q
+ 
 ################################
 
 !wget http://bit.ly/3ZLyF82 -O CSS.css -q
-    
+
 from IPython.core.display import HTML
 with open('./CSS.css', 'r') as file:
     custom_css = file.read()
-
 HTML(custom_css)
 
 ################################
