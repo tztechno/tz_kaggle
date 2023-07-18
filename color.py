75634531a56a18d631a45ac88687fb8a2d4835e5
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-18 15:51:37 +0900

    Update color.py

--- color.py
+++ color.py
@@ -14,10 +14,25 @@ cell_colors = [
 
 #############################################
 
-# HEX形式のカラーコード
-hex_color = '#FF0000'  # RED
+#HEX CORDS
+color_codes = [
+    '#FF0000',  # Red
+    '#FF4500',  # OrangeRed
+    '#FF8C00',  # DarkOrange
+    '#FFA500',  # Orange
+    '#FFD700',  # Gold
+    '#FFFF00',  # Yellow
+    '#ADFF2F',  # GreenYellow
+    '#7FFF00',  # Chartreuse
+    '#00FF00',  # Lime
+    '#00FA9A',  # MediumSpringGreen
+    '#00FFFF',  # Cyan
+    '#00BFFF',  # DeepSkyBlue
+    '#0000FF',  # Blue
+    '#8A2BE2'   # BlueViolet
+]
 
-# HEX形式をBGR形式に変換
-bgr_color = tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
+hexc=color_codes[int(datai.iloc[j,5])%len(color_codes)]
+c = tuple(int(hexc[i:i+2], 16) for i in (1, 3, 5))
 
 #############################################
