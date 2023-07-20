16a7077b999e72271e225223f929c7d79e3465ab
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-20 20:31:12 +0900

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,15 @@
+#########################################################
+
 #install into /kaggle/working/
 
 !git clone https://github.com/JonathonLuiten/TrackEval.git /kaggle/working/TrackEval
+
+#########################################################
+
+!pip install pystan==2.19.1.1
+!pip install fbprophet
+import sys
+sys.path.append('../input/multi-prophet/multi-prophet-master')
+from multi_prophet import MultiProphet
+
+#########################################################
