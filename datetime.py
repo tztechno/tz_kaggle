fc835bbfd0ad34c7a2ff9d035bd30443340c0d85
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-24 08:45:01 +0900

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,3 +1,6 @@
+
+####################################################
+
 import pandas as pd
 
 # 'date'列を日付型に変換
@@ -5,3 +8,10 @@ train2['date'] = pd.to_datetime(train2['date'])
 
 # 'date'列の型を確認
 print(train2['date'].dtype)
+
+####################################################
+
+df["aired_start_date"] = df["aired"].apply(lambda x: pd.to_datetime(x.split("to")[0], errors="coerce"))
+df["aired_end_date"] = df["aired"].apply(lambda x: pd.to_datetime(x.split("to")[-1], errors="coerce"))
+
+####################################################
