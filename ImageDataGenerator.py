59111c3b240102729972eb9e1c4e278b5df66857
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-27 21:28:52 +0900

    Update ImageDataGenerator.py

--- ImageDataGenerator.py
+++ ImageDataGenerator.py
@@ -1,3 +1,5 @@
+#########################################################################
+
 from keras.preprocessing.image import ImageDataGenerator
 
 # ImageDataGeneratorのインスタンスを作成
@@ -22,3 +24,28 @@ model.fit_generator(
     steps_per_epoch=total_train_samples // batch_size,  # 1エポックごとのステップ数
     epochs=num_epochs
 )
+
+#########################################################################
+
+datagen = ImageDataGenerator(rescale=1.0/255,
+                                  zoom_range=0.2,
+                                  shear_range=0.2,
+                                  rotation_range=10,
+                                  width_shift_range=0.2,
+                                  height_shift_range=0.2,
+                                  horizontal_flip=True,
+                                  fill_mode='nearest')
+
+train_ds = keras.utils.image_dataset_from_directory(
+    directory = '/kaggle/input/rock-paper-scissor/rps/rps',
+    image_size = (224, 224),
+    seed = seed_train_validation,
+    color_mode = 'rgb',
+    label_mode = 'categorical',
+    batch_size = 32,
+    shuffle = True)
+
+train_ds = train_ds.map(lambda x, y: (datagen.flow(x, batch_size=32, shuffle=False).next(), y))
+train_ds = train_ds.take(limit_train_data)
+
+#########################################################################
