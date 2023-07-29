46bd808f2d14599f1adbb1b5bef235d0e1130faa
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-29 11:02:42 +0900

    Create split.py

---
+++ split.py
@@ -0,0 +1,20 @@
+
+trainval_ds = keras.utils.image_dataset_from_directory(
+    directory ='/kaggle/input/rock-paper-scissor/rps/rps',
+    image_size = (224, 224),
+    seed = seed_train_validation,
+    color_mode='rgb',
+    label_mode = 'categorical',
+    batch_size=16,
+    shuffle = True)
+
+data_list = list(trainval_ds.as_numpy_iterator())
+images = np.concatenate([data[0] for data in data_list])
+labels = np.concatenate([data[1] for data in data_list])
+
+train_images, val_images, train_labels, val_labels = train_test_split(images, labels, test_size=0.3, random_state=seed_train_validation)
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).batch(16).shuffle(True)
+val_ds = tf.data.Dataset.from_tensor_slices((val_images, val_labels)).batch(16)
+
+################################################################################
