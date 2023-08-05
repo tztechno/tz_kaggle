746f9ca200685cc41dc3cacea77c96f1ea26055f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-05 14:55:51 +0900

    Update warnings.py

--- warnings.py
+++ warnings.py
@@ -1,5 +1,13 @@
+########################################
+
 import warnings
 
-# 特定の警告を非表示にする
 warnings.filterwarnings("ignore", category=UserWarning)  # Dataset has 0 variance
 warnings.filterwarnings("ignore", category=FutureWarning)  # shade is now deprecated
+
+########################################
+
+import warnings
+warnings.filterwarnings("ignore")
+
+########################################
