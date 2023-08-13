0e300460300f3944045af39e94293c385db198f5
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-13 18:33:03 +0900

    Update parquet.py

--- parquet.py
+++ parquet.py
@@ -1,6 +1,21 @@
+
+#################################################################
+
 import pyarrow as pa
 import pyarrow.parquet as pq
 
 path='d3/'+str(pid)+'_'+str(sid)+'.parquet'
 tensor = pa.Tensor.from_numpy(A[1:])
 pq.write_tensor(tensor,path)   
+
+#################################################################
+
+parquet_file_path = 'your_file.parquet'
+
+# Read the Parquet file into a DataFrame
+df = pd.read_parquet(parquet_file_path)
+
+# Now you can work with the DataFrame 'df'
+print(df.head())  # Display the first few rows of the DataFrame
+
+#################################################################
