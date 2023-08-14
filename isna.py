79ea9679c6255f019a8de84eb7a7f39cc0167a08
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-14 10:05:17 +0900

    Update isna.py

--- isna.py
+++ isna.py
@@ -1,3 +1,6 @@
+
+###############################################
+
 train = data1[data1['x_e_out [-]'].notna()]
 test = data1[data1['x_e_out [-]'].isna()]
 
@@ -6,7 +9,6 @@ test = data1[data1['x_e_out [-]'].isna()]
 df_numeric.info()
 df_numeric.isnull().sum()
 df_numeric.dropna(inplace=True)
-
 
 ###############################################
 
