aa0c28e5392576bb907be551587bea55a434566a
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-17 22:12:33 +0900

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -1,2 +1,14 @@
+##################################################
+
 from pydub import AudioSegment
 display(AudioSegment.from_file('/kaggle/input/bengaliai-speech/train_mp3s/000005f3362c.mp3'))
+
+##################################################
+
+import librosa
+
+path0 = 'path/to/your/audio/file.mp3'
+y, sr = librosa.load(path0)
+
+##################################################
+
