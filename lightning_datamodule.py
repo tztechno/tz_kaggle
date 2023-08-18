c8098c441bf5bd18c44256231303320f0eca0079
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-18 17:41:33 +0900

    Update lightning_datamodule.py

--- lightning_datamodule.py
+++ lightning_datamodule.py
@@ -136,3 +136,56 @@ class DataModule(pl.LightningDataModule):
         return self.test_dataset
 
 ###################################################   
+### in case of no image folder
+
+class ImageDataset(Dataset):
+    def __init__(self, path_label, transform=None):
+        self.path_label = path_label
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.path_label)
+
+    def __getitem__(self, idx):
+        path, label = self.path_label[idx]
+        img = cv2.imread(path) 
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)     
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, label
+
+class DataModule(pl.LightningDataModule):
+    
+    def __init__(self, transform=transform, batch_size=32):
+        super().__init__()
+        self.root_dir = "/kaggle/input/waveform-images-of-bengali-sound"
+        self.transform = transform
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        dataset = ImageDataset(path_label)
+        n_data = len(dataset)
+        print(n_data)
+        n_train = int(0.5 * n_data)
+        n_valid = int(0.2 * n_data)
+        n_test = n_data - n_train - n_valid
+
+        train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [n_train, n_valid, n_test])
+
+        self.train_dataset = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        self.val_dataset = DataLoader(val_dataset, batch_size=self.batch_size)
+        self.test_dataset = DataLoader(test_dataset, batch_size=self.batch_size)
+        
+    def train_dataloader(self):
+        return self.train_dataset
+    
+    def val_dataloader(self):
+        return self.val_dataset
+    
+    def test_dataloader(self):
+        return self.test_dataset
+
+###################################################   
+
