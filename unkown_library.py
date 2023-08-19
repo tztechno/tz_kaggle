4cc4c9b80417dcdf5b0547c3c6053fdae5420483
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-19 22:48:31 +0900

    Update unkown_library.py

--- unkown_library.py
+++ unkown_library.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from functools import partial
 from dataclasses import dataclass, field
 import pyctcdecode
-import pyctcdecode
 import kenlm
 from transformers import Wav2Vec2Processor, Wav2Vec2ProcessorWithLM, Wav2Vec2ForCTC
 from bnunicodenormalizer import Normalizer
