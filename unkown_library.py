187f75fec60ae68b1f39f453dbca267ea3eba7fb
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-21 15:37:54 +0900

    Update unkown_library.py

--- unkown_library.py
+++ unkown_library.py
@@ -1,13 +1,20 @@
 import typing as tp
+from typing import Any, Dict, List, Union
+
 from pathlib import Path
 from functools import partial
 from dataclasses import dataclass, field
 import pyctcdecode
 import kenlm
-from transformers import Wav2Vec2Processor, Wav2Vec2ProcessorWithLM, Wav2Vec2ForCTC
 from bnunicodenormalizer import Normalizer
 import cloudpickle as cpkl
+from peft import LoraConfig, PeftModel, LoraModel, LoraConfig, get_peft_model
 
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+from transformers import TrainingArguments
+from transformers import TrainerState, TrainerControl, TrainerCallback
+from transformers import WhisperFeatureExtractor
+from transformers import Wav2Vec2Processor, Wav2Vec2ProcessorWithLM, Wav2Vec2ForCTC
 
 
 
