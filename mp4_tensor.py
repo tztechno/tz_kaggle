4be6e58a207ecd66dc9da9ddf82e31506a572145
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-04 18:56:52 +0900

    Update mp4_tensor.py

--- mp4_tensor.py
+++ mp4_tensor.py
@@ -1,3 +1,14 @@
+###############################################
+
 import tensorflow as tf
 video_tensor = tf.io.read_file('output.mp4')
 tensor = tf.image.decode_video(video_tensor)
+
+###############################################
+
+import ffmpeg
+video_stream = ffmpeg.input('my_video.mp4')
+frame_tensors = [ffmpeg.frame_to_tensor(frame) for frame in video_stream.frames]
+print(frame_tensors)
+
+###############################################
