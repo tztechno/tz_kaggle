99e261f97673b6769469bb62e3d05995f1ab3040
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-05 15:21:39 +0900

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -4,6 +4,13 @@ mapping_dict = movies.set_index("Movie_ID")["Name"].to_dict()
 
 ################
 
+idx2eco = data0['economy'].to_dict()
+print(idx2eco)
+eco2idx = {v: k for k, v in idx2eco.items()}
+print(eco2idx)
+
+################
+
 Name=data['Species'].unique().tolist()
 N=list(range(len(Name)))
 normal_mapping=dict(zip(Name,N)) 
@@ -18,5 +25,4 @@ normal_mapping = dict(zip(class_names, N))
 reverse_mapping = dict(zip(N, class_names))    
 
 ################
-
 
