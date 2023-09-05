192aef05786aec9766a649f6030f9c540f8ddb29
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-05 13:55:34 +0900

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,14 +1,14 @@
+
+##################################################
+
 import os
 
 def tree(directory, level=0):
     prefix = "|   " * level
-    prefix += "|-- "
-    
-    contents = os.listdir(directory)
-    
+    prefix += "|-- "    
+    contents = os.listdir(directory)    
     for item in sorted(contents):
-        item_path = os.path.join(directory, item)
-        
+        item_path = os.path.join(directory, item)        
         if os.path.isdir(item_path):
             print(prefix + item + "/")
             tree(item_path, level+1)
@@ -18,3 +18,9 @@ def tree(directory, level=0):
 # フォルダのパスを指定して実行する
 folder_path = "/path/to/folder"
 tree(folder_path)
+
+##################################################
+
+!tree -I *.npz /kaggle/input/predict-ai-model-runtime/
+
+##################################################
