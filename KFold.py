98ffef30d836ab96dec9f8dfe2dd37a9584aaa86
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-07 15:41:54 +0900

    Update KFold.py

--- KFold.py
+++ KFold.py
@@ -1,4 +1,21 @@
+    
+######################################################
 
+from sklearn.model_selection import KFold
+
+X =dataX
+y = dataY
+kf = KFold(n_splits=5)
+y_test_results = []
+
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    model = RandomForestRegressor(random_state = 10, max_features='sqrt')
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    y_test_results.extend(y_pred)
+    
 ######################################################
 
 import numpy as np
