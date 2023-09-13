f543c4e497722ce1de8f653692a6c76b9f7b9f2f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-13 16:41:57 +0900

    Update clip.py

--- clip.py
+++ clip.py
@@ -4,3 +4,22 @@ def convert_to_int_with_range_limit(value, min_value, max_value):
     int_value = int(value)
     int_value = np.clip(int_value, min_value, max_value)
     return int_value
+
+#########################################
+
+x = 101
+print(clip(x, 0, 100))
+
+#########################################
+
+import numpy as np
+x = 101
+print(np.clip(x, 0, 100))    
+
+#########################################
+
+import torch
+x = torch.tensor(101)
+print(torch.clip(x, 0, 100))    
+
+#########################################
