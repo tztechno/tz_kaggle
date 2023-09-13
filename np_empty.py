e95a33fc83cbb67a18bb7eac3c10ae27bd7ab303
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-13 19:12:14 +0900

    Create np_empty.py

---
+++ np_empty.py
@@ -0,0 +1,5 @@
+'''
+np.empty((0, 512, 512, 3)) is not the same as np.zeros((0, 512, 512, 3)). 
+The former creates an empty array with an unspecified initial content, 
+while the latter creates an array with all values initialized to zero. 
+'''
