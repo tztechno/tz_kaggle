5e8a363848501f34c3721270f0307356374e0d7f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-17 09:32:44 +0900

    Update html.py

--- html.py
+++ html.py
@@ -1,30 +1,30 @@
 
-########################
+################################################
 
 <p style="font-family:JetBrains Mono; font-weight:bold; letter-spacing: 2px; 
 color:#006600; font-size:140%; text-align:left;padding: 0px; 
 border-bottom: 3px solid #003300"> xxxxxx </p>
 
-########################
+################################################
 
 <div style="border-radius:10px; border:#DEB887 solid; padding: 15px; 
 background-color: #FFFAF0; font-size:100%; text-align:left">
 <h3 align="left"><font color='#DEB887'> xxxxx </font></h3>
 
-########################
+################################################
 
 <div style="border-radius:10px; border:#20B2AA solid; padding: 15px; 
 background-color: #E0FFFF; font-size:100%; text-align:left">
 <h3 align="left"><font color='#DEB887'> xxxxx </font></h3>
 
-########################
+################################################
 
 <div style="background-color:#F0E3D2; color:#19180F; font-size:15px; 
 font-family:Verdana; padding:10px; border: 2px solid #19180F; border-radius:10px"> 
 📌
 Importing modules    </div>
 
-########################
+################################################
 
 <a id="3"></a>
 <div style="text-align: center; background: #1ED760; font-family: 'Trebuchet MS', Arial, sans-serif; 
@@ -32,12 +32,15 @@ color: white; padding: 15px; font-size: 26px; font-weight: bold;
 line-height: 1; border-radius: 50% 0 50% 0 / 40px; margin-bottom: 20px; 
 box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);">3. Face Embeddings Generation</div>
 
-########################
+################################################
 
 ### <p style="font-family:JetBrains Mono; font-weight:bold; letter-spacing: 2px; 
 color:#006600; font-size:140%; text-align:left;padding: 0px; border-bottom: 3px solid #003300"> 
 Why is it required to store embeddings?</p>
 
-########################
+################################################
 
-########################
+<center> <a href="#top" role="button" aria-pressed="true" >⬆️Back to the Top ⬆️</a>
+
+################################################
+
