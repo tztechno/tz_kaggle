99cae4de630cefcd60bf091b5dbca2b0f780fee6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-19 14:04:45 +0900

    Update time.py

--- time.py
+++ time.py
@@ -1,10 +1,13 @@
+#################################
+
 import time
-
 tstart = time.time()
-
-# Your code or task to be measured for execution time goes here
-
 tend = time.time()
 elapsed_time = tend - tstart
-
 print(f"Elapsed time: {elapsed_time} seconds")
+
+#################################
+
+%%time
+
+#################################
