137960c34d53d76e053606075167a99d189e90ba
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-06 14:23:31 +0900

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -6,7 +6,23 @@ Image(open('./detections.gif','rb').read())
 
 ####################################################################
 
+# animation.gif to mp4
+import imageio_ffmpeg as ffmpeg
+import imageio
 
+# 入力と出力ファイルのパスを指定
+input_gif_path = 'input.gif'
+output_mp4_path = 'output.mp4'
+
+# GIFファイルを読み込み、MP4に変換
+with imageio.get_reader(input_gif_path) as reader:
+    fps = reader.get_meta_data()['fps']  # GIFのフレームレートを取得
+    writer = imageio.get_writer(output_mp4_path, fps=fps)
+    for frame in reader:
+        writer.append_data(frame)
+    writer.close()
+
+print(f'{input_gif_path} を {output_mp4_path} に変換しました。')
 
 
 ####################################################################
