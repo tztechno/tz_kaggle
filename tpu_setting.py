6cef68902b20bbe467dfea0f4605d92b83accd5f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-21 18:41:42 +0900

    Create tpu_setting.py

---
+++ tpu_setting.py
@@ -0,0 +1,32 @@
+
+def get_strategy(device='TPU'):
+    if "TPU" in device:
+        tpu = 'local' if device=='TPU-VM' else None
+        print("connecting to TPU...")
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect(tpu=tpu)
+        strategy = tf.distribute.TPUStrategy(tpu)
+        IS_TPU = True
+
+    if device == "GPU"  or device=="CPU":
+        ngpu = len(tf.config.experimental.list_physical_devices('GPU'))
+        if ngpu>1:
+            print("Using multi GPU")
+            strategy = tf.distribute.MirroredStrategy()
+        elif ngpu==1:
+            print("Using single GPU")
+            strategy = tf.distribute.get_strategy()
+        else:
+            print("Using CPU")
+            strategy = tf.distribute.get_strategy()
+        IS_TPU = False
+
+    if device == "GPU":
+        print("Num GPUs Available: ", ngpu)
+
+    AUTO     = tf.data.experimental.AUTOTUNE
+    REPLICAS = strategy.num_replicas_in_sync
+    print(f'REPLICAS: {REPLICAS}')
+
+    return strategy, REPLICAS, IS_TPU
+
+STRATEGY, N_REPLICAS, IS_TPU = get_strategy('TPU-VM')
