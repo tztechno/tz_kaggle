dbf71614d169d0e62b6ed3d2cc4e2df3a2131319
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-21 17:09:25 +0900

    Update unkown_library.py

--- unkown_library.py
+++ unkown_library.py
@@ -1,13 +1,24 @@
+
 import pyctcdecode
 import kenlm
-
 import typing as tp
 from typing import Any, Dict, List, Optional, Union
-
 from pathlib import Path
-
 from functools import partial
+from dataclasses import dataclass, field
+from bnunicodenormalizer import Normalizer
+import cloudpickle as cpkl
+from peft import LoraConfig, PeftModel, LoraModel, LoraConfig, get_peft_model
+from abc import ABC, abstractmethod
+from IPython.display import FileLink
+import joblib
+import whisper
+import soundfile 
+import jiwer
+from concurrent.futures import ThreadPoolExecutor
 
+
+#keras
 from keras.models import Model
 from keras.layers import Activation
 from keras.layers import BatchNormalization
@@ -21,12 +32,9 @@ from keras.layers import Lambda
 from keras.layers import MaxPooling2D
 from keras.layers import add
 from keras import backend as K
-from dataclasses import dataclass, field
 
-from bnunicodenormalizer import Normalizer
-import cloudpickle as cpkl
-from peft import LoraConfig, PeftModel, LoraModel, LoraConfig, get_peft_model
 
+#transformers
 from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 from transformers import TrainingArguments
 from transformers import TrainerState, TrainerControl, TrainerCallback
@@ -34,19 +42,15 @@ from transformers import WhisperFeatureExtractor
 from transformers import Wav2Vec2Processor, Wav2Vec2ProcessorWithLM, Wav2Vec2ForCTC
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-import torchaudio
-import torchaudio.transforms as tat
 
+#sklearn
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_absolute_error, make_scorer
 from sklearn.preprocessing import LabelEncoder,Normalizer
 from sklearn.svm import SVC
 
-from abc import ABC, abstractmethod
 
-from IPython.display import FileLink
-import joblib
-
+#keras
 from keras.models import Model
 from keras.layers import Activation
 from keras.layers import BatchNormalization
@@ -56,3 +60,12 @@ from keras.layers import Input
 from keras.layers import Lambda
 from keras.layers import add
 from keras import backend as K
+
+
+#torch
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+import torch.multiprocessing as mp
+import torchaudio
+import torchaudio.transforms as tat
+
