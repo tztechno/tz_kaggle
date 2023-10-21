3714663ec0f7fe9c8768e86dec31803c596d7706
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-21 18:43:01 +0900

    Update tpu_setting.py

--- tpu_setting.py
+++ tpu_setting.py
@@ -30,3 +30,19 @@ def get_strategy(device='TPU'):
     return strategy, REPLICAS, IS_TPU
 
 STRATEGY, N_REPLICAS, IS_TPU = get_strategy('TPU-VM')
+
+
+##############################################
+
+import tensorflow as tf
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
+
+
