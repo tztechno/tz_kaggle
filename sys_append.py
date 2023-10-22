95d6a2a77b6f8f8645565455ad3dcfb9daf1d078
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-22 12:37:27 +0900

    Update sys_append.py

--- sys_append.py
+++ sys_append.py
@@ -1,4 +1,15 @@
 
+###########################################
+
 import sys
 sys.path.append('../input/multi-prophet/multi-prophet-master')
 from multi_prophet import MultiProphet
+
+###########################################
+
+sys.path.append("/kaggle/input/detection-wheel")
+
+#/kaggle/input/detection-wheelディレクトリ内にmy_module.pyというPythonモジュールがある場合
+import my_module
+
+###########################################
