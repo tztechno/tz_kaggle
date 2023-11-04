7df20c15707d4c7a1cf3e0ca8a69bfcabaa51c13
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-04 11:07:37 +0900

    Update yaml.py

--- yaml.py
+++ yaml.py
@@ -12,3 +12,13 @@ with open('plate.yaml', 'w') as outfile:
     yaml.dump(plate_yaml, outfile, default_flow_style=True)
     
 %cat plate.yaml
+
+
+########################################
+
+import yaml
+
+data = {"key1": "value1", "key2": "value2"}
+
+with open("data.yaml", "w") as yaml_file:
+    yaml.dump(data, yaml_file)
