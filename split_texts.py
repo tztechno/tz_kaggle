d7008f0fe7338ffa260ebdf9545e4bac401406e6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-10 20:08:11 +0900

    Update split_texts.py

--- split_texts.py
+++ split_texts.py
@@ -22,3 +22,27 @@ df2.columns=['text','target']
 df3=pd.concat([df1,df2],axis=0)
 display(df3)
 train=df3
+
+####################################
+
+
+def split_text(df):
+    df['text1']=''
+    df['text2']=''
+    for i in range(len(df)):
+        texts=df.loc[i,'text'].replace('\n','')
+        cts=texts.count('.')
+        tex=texts.split('.')
+        for t1 in tex[0:cts//2]:
+            df.iloc[i,2]+=t1
+        for t2 in tex[cts//2:-1]:
+            df.iloc[i,3]+=t2
+    df1=df[['text1','target']]
+    df2=df[['text2','target']]
+    df1.columns=['text','target']
+    df3=pd.concat([df1,df2],axis=0)
+    return df3
+
+
+
+
