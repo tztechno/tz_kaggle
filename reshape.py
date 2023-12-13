a754d5b7364a401ed9bbc1fafc887e8ca6f60e36
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-13 22:10:09 +0900

    Update reshape.py

--- reshape.py
+++ reshape.py
@@ -1,1 +1,5 @@
-.detach()[:,1].reshape((-1))
+from_pretrained(model_name,num_labels=2)
+output = output["logits"].detach()[:,1].reshape((-1))
+
+from_pretrained(model_name,num_labels=1)
+output = output["logits”].squeeze(-1)
