accd2832c2999036d39629b13a084ad916b02341
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-13 22:16:03 +0900

    Update reshape.py

--- reshape.py
+++ reshape.py
@@ -1,5 +1,7 @@
 from_pretrained(model_name,num_labels=2)
 output = output["logits"].detach()[:,1].reshape((-1))
 
+
+#/kaggle/input/roberta-base
 from_pretrained(model_name,num_labels=1)
 output = output["logits”].squeeze(-1)
