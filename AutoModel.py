089924162bd1aaa524a1311eda273c9cc0f9f624
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-13 23:09:14 +0900

    Update AutoModel.py

--- AutoModel.py
+++ AutoModel.py
@@ -5,6 +5,18 @@ model_name = "roberta-base"
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)   
 
+
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoConfig
+
+model_name='/kaggle/input/transformers-roberta-base'
+tokenizer = AutoTokenizer.from_pretrained(model_name)   
+config = AutoConfig.from_pretrained(model_name)
+config.num_labels = 1
+model = AutoModelForSequenceClassification.from_pretrained(model_name, config=config, ignore_mismatched_sizes=True)
+
+
+
 #tokenizer = transformers.RobertaTokenizer.from_pretrained("/kaggle/input/roberta-base")
 #tokenizer = transformers.RobertaTokenizer.from_pretrained("roberta-base")
 #model = AutoModelForSequenceClassification.from_pretrained(model_name)
