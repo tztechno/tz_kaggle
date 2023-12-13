0b5c8b4f0efd40464ff38add1eb98ed8a5a58d2e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-13 15:01:37 +0900

    Update loss_fn.py

--- loss_fn.py
+++ loss_fn.py
@@ -3,5 +3,12 @@ def loss_fn(output, target):
     if output.shape != target.shape:
         # Perform necessary operations to make them compatible (e.g., reshape)
         target = target.view_as(output)
-
     return torch.sqrt(nn.MSELoss()(output, target))
+
+
+def loss_fn(output, target):
+    print("Output shape:", output.shape)
+    print("Target shape:", target.shape)
+    return torch.sqrt(nn.MSELoss()(output, target))
+
+
