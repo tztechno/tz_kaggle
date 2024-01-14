4b9a33a37190630f0f3ecd13dba52b299e5db671
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-14 19:17:18 +0900

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -1,3 +1,19 @@
+
+######################################
+
+def mkdir_if_not_exist(folder_path, overwrite=False):
+    if os.path.exists(folder_path):
+        if overwrite:
+            shutil.rmtree(folder_path)
+            os.makedirs(folder_path)
+    else:
+        os.makedirs(folder_path)
+
+
+def rmdir_if_exist(folder_path):
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+
 ######################################
 
 os.makedirs('datasets', exist_ok=True)
