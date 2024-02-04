09e65579003660f5ec0c70b1906114e688afa28a
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-05 01:33:24 +0900

    Update yolo_nas.py

--- yolo_nas.py
+++ yolo_nas.py
@@ -4,3 +4,9 @@ def collate_fn(batch):
     labels = torch.stack([label for label in labels], dim=0)
     images = A.Resize(0.1, 0.1)(images=images)
     return images, labels
+
+
+
+# old checkpoint_path=os.path.join(config.CHECKPOINT_DIR, config.EXPERIMENT_NAME, 'average_model.pth')
+
+# now checkpoint_path=os.path.join(trainer.checkpoints_dir_path, "average_model.pth")
