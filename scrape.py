7e98d903ff84aadfc85270fab1492401b117d1ce
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-07 15:48:29 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,3 +1,5 @@
+######################################################
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,3 +10,33 @@ soup = BeautifulSoup(source,'lxml')
 response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
+
+######################################################
+
+#tags=soup.find_all('img')
+#print(tags)
+
+for tag in tags:
+    image_url = tag['src']
+    image_name = os.path.join('images', os.path.basename(image_url))
+
+    try:
+        # Send a GET request to the image URL
+        response = requests.get(image_url, stream=True)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Open a local file with write-binary mode
+            with open(image_name, 'wb') as file:
+                # Iterate over the content of the response and write it to the local file
+                for chunk in response.iter_content(chunk_size=128):
+                    file.write(chunk)
+
+            print(f"Image '{image_name}' saved successfully.")
+        else:
+            print(f"Failed to download image from '{image_url}'. Status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"Error downloading image from '{image_url}': {e}")
+
+######################################################
