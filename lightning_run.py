1ca14377f3f9ab1d33e6a6afb6f337166c7b168a
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-01 23:39:49 +0900

    Create lightning_run.py

---
+++ lightning_run.py
@@ -0,0 +1,31 @@
+################################
+[train,fit]
+
+dataset = ImageDataset(path_label)
+dataset.setup() 
+train_dataloader = dataset.train_dataloader
+val_dataloader = dataset.val_dataloader
+
+datamodule = DataModule()
+datamodule.setup() 
+model = ConvolutionalNetwork()
+
+trainer = pl.Trainer(max_epochs=2)
+trainer.fit(model, datamodule)
+val_loader = datamodule.val_dataloader()
+trainer.test(dataloaders=val_loader)
+
+
+[test,predict]
+
+tdataset = ImageDataset(tpath_label)
+tdataset.setup(stage='Test') 
+test_dataloader = tdataset.test_dataloader
+
+datamodule = DataModule()
+datamodule.setup(stage='Test') 
+test_loader = datamodule.test_dataloader()
+
+trainer.test(dataloaders=test_loader)
+
+################################
