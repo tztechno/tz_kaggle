4640be237c7b8ac762d3d0e49252c983e9c98504
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-01 11:59:10 +0900

    Update labelencoder.py

--- labelencoder.py
+++ labelencoder.py
@@ -1,4 +1,4 @@
-
+--------------------------------------------------------------------------
 from sklearn.preprocessing import LabelEncoder
 
 def labelencoder(df):
@@ -9,3 +9,18 @@ def labelencoder(df):
             lbl.fit(list(df[c].values))
             df[c] = lbl.transform(df[c].values)
     return df
+
+--------------------------------------------------------------------------
+from sklearn.preprocessing import LabelEncoder
+
+def labelencoder(df_train, df_test):
+    combined_df = pd.concat([df_train, df_test])
+    for c in combined_df.columns:
+        if combined_df[c].dtype == 'object':
+            combined_df[c] = combined_df[c].fillna('N')
+            lbl = LabelEncoder()
+            lbl.fit(list(combined_df[c].values))
+            combined_df[c] = lbl.transform(combined_df[c].values)
+    return combined_df.iloc[:len(df_train)], combined_df.iloc[len(df_train):]
+
+--------------------------------------------------------------------------
