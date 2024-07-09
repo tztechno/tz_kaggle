f7516dee699facf6caaa385fe0fdc56382216070
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-09 11:17:35 +0900

    Update instance.py

--- instance.py
+++ instance.py
@@ -1,5 +1,16 @@
 #インスタンス一覧
 
+import sys
+import gc
+
+for obj in gc.get_objects():
+    if isinstance(obj, pd.DataFrame):
+        print(f"DataFrame: {obj.shape}, Memory usage: {obj.memory_usage().sum() / 1e6:.2f} MB")
+
+---------------------------------------------
+
 !cat /proc/cpuinfo
 !cat /proc/meminfo
 !df -h
+
+---------------------------------------------
