cd02c01fc7d8e07ae3c64435aa97f0dc3c7e9f52
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-09-24 15:24:23 +0900

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -2,12 +2,7 @@
 
 from sklearn.utils import shuffle
 
-# Assuming you have your data and labels in separate arrays or lists
-data = [...]  # Your data, e.g., a list of feature vectors
-labels = [...]  # Your corresponding labels, e.g., a list of target values
-
-# Shuffle the data and labels together while maintaining the correspondence between them
-data_shuffled, labels_shuffled = shuffle(data, labels, random_state=42)  # You can use any random_state value you like
+data_shuffled, labels_shuffled = shuffle(data, labels, random_state=42) 
 
 ############################################################
 
@@ -17,3 +12,18 @@ dff = shuffle(df)
 df = dff.sample(frac=0.10, replace=False, random_state=1)#Change the rate of f
 
 ############################################################
+
+shuffled_df_with_seed = df.sample(frac=1, random_state=42).reset_index(drop=True)
+
+print(shuffled_df_with_seed)
+
+#########################################################
+
+import pandas as pd
+
+df = pd.DataFrame({'A': [1, 2, 3, 4, 5],
+                   'B': ['a', 'b', 'c', 'd', 'e']})
+
+shuffled_df = df.sample(frac=1).reset_index(drop=True)
+
+#########################################################
