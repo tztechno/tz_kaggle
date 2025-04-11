7e4abfb014969acb914faaf588b75249ce28be68
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-04-11 10:28:27 +0900

    Update wandb.py

--- wandb.py
+++ wandb.py
@@ -1,4 +1,17 @@
 
+from kaggle_secrets import UserSecretsClient
+import wandb
+user_secrets = UserSecretsClient()
+secret_value = user_secrets.get_secret("wandb_api_key")
+wandb.login(key=secret_value)
+
+# save metrics into wandb folder
+import os
+os.environ["WANDB_DIR"] = "./wandb"
+wandb.init(project="open-r1", mode="online")
+
+-------------------------
+
 # Wandb login:
 from kaggle_secrets import UserSecretsClient
 import wandb
