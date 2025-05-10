2153e23fd09c63b4aed07d3840b0b9b47c78b590
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-05-10 11:45:20 +0900

    Update time.py

--- time.py
+++ time.py
@@ -1,3 +1,13 @@
+
+#################################
+from datetime import datetime
+import pytz  # タイムゾーン処理用ライブラリ
+
+# 現在時刻をUTCで取得
+now_utc = datetime.now(pytz.utc)
+print(f"UTC: {now_utc}")
+
+
 #################################
 
 import time
