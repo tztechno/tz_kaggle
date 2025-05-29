071f3d2370eaf498f2face7aff622581d60bc15c
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-05-29 11:43:48 +0900

    Create colab_kagglelogin.py

---
+++ colab_kagglelogin.py
@@ -0,0 +1,17 @@
+# IMPORTANT: SOME KAGGLE DATA SOURCES ARE PRIVATE
+# RUN THIS CELL IN ORDER TO IMPORT YOUR KAGGLE DATA SOURCES.
+import kagglehub
+kagglehub.login()
+
+ stpeteishii
+ Kaggle API key required
+
+# IMPORTANT: RUN THIS CELL IN ORDER TO IMPORT YOUR KAGGLE DATA SOURCES,
+# THEN FEEL FREE TO DELETE THIS CELL.
+# NOTE: THIS NOTEBOOK ENVIRONMENT DIFFERS FROM KAGGLE'S PYTHON
+# ENVIRONMENT SO THERE MAY BE MISSING LIBRARIES USED BY YOUR
+# NOTEBOOK.
+
+stpeteishii_embedding_images_private_path = kagglehub.dataset_download('stpeteishii/embedding-images-private')
+
+print('Data source import complete.')
