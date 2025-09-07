9cfa3f464fe312e1d7aa31ee8dfdb8b4f3a05f0f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-09-08 03:04:46 +0900

    Update oof.py

--- oof.py
+++ oof.py
@@ -2,3 +2,15 @@
 oof = final_model.predict(X, num_iteration=final_model.best_iteration_)
 np.save('oof.npy',oof)
 ----------------------------------
+X_tensor = torch.tensor(X.values, dtype=torch.float32)
+
+if torch.cuda.is_available():
+    X_tensor = X_tensor.cuda()
+    model = model.cuda()
+
+model.eval()
+with torch.no_grad():
+    oof = model(X_tensor).squeeze().cpu().numpy()
+
+np.save('oof.npy', oof)
+----------------------------------
